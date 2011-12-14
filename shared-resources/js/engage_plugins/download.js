f1327fc362a17635dc07a56d0599d0d29dcdc5ab
denmeyer <denmeyer@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-12-14 09:53:41 +0000

    MH-7135 Added missing file.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@11457 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ shared-resources/js/engage_plugins/download.js
@@ -0,0 +1,124 @@
+/**
+ *  Copyright 2011 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+var Opencast = Opencast || {};
+
+/**
+ * @namespace the global Opencast namespace download
+*/
+Opencast.download = (function() {
+
+    var that = {};
+
+    /**
+     * @memberOf Opencast.download
+     * @description Shows download links for files
+    */
+
+    that.showLinks = function() {
+
+        $.ajax(
+            {
+            url: Opencast.engage.getSearchServiceEpisodeJsonURL(),
+            data: "id=" + Opencast.Player.getMediaPackageId(),
+            dataType: 'json',
+            success: function(data)
+            {
+                var i,
+                    tracks = data["search-results"] ? data["search-results"]["result"]["mediapackage"]["media"]["track"] : [],
+                    video_files = [],
+                    audio_files = [],
+                    video_markup = "",
+                    audio_markup = "";
+
+                    var buildLinks = function(media){
+                        var myHTML = "",
+                        media_href,
+                        media_type,
+                        media_resolution,
+                        media_description,
+                        media_size,
+                        j,
+                        DISPLAY_DESCRIPTION = [
+                           ["1280x720", "High Definition"],
+                           ["1024x768", "High Resolution"],
+                           ["320x240", "Low Resolution"]
+                        ];
+
+                        for( i = 0; i < media.length; i+=1 ){
+
+                            media_href = media[i].url;
+                            media_type = media[i].type;
+                            media_type = (media_type === 'presenter/delivery') ? "Presenter " : "Presentation ";
+                            media_resolution = media[i].video ? media[i].video.resolution : " ";
+                            media_description = " ";
+
+                            for( j = 0; j < DISPLAY_DESCRIPTION.length; j+=1 ){
+                                if (DISPLAY_DESCRIPTION[j][0] === media_resolution){
+                                    media_description = DISPLAY_DESCRIPTION[j][1];
+                                }
+                            }
+
+                            media_format = media_href.split(".");
+                            // get size of media
+                            media_size = Opencast.download.getMediaSize(media_href);
+                            media_size = media_size + "MB"
+                            myHTML += " <a href="+ media_href + ">" +  media_type + " - " + media_description  + " [ File extension : <em>"+  media_format[media_format.length - 1] +"</em> ] " + media_size + "</a></br>";
+                        }
+
+                        return myHTML;
+                    };
+
+                // Separate Video and Audio files
+                for( i = 0; i < tracks.length; i+=1 ){
+                    tracks[i].video ? video_files.push(tracks[i]) : audio_files.push(tracks[i]);
+                }
+
+                video_markup = buildLinks(video_files);
+                audio_markup = buildLinks(audio_files);
+
+                $('#oc_client_downloads').append("<span><b>Video Files</b></span><br>");
+                $('#oc_client_downloads').append("<div id=\"oc_download_video\"></div>");
+                (video_files.length > 0) ?  $('div#oc_download_video').html(video_markup) : $('div#oc_download_video').html("No video files present for this media.");
+
+                $('#oc_client_downloads').append("</br><span><b>Audio Files<b></span><br>");
+                $('#oc_client_downloads').append("<div id=\"oc_download_audio\"></div>");
+                (audio_files.length > 0) ? $('div#oc_download_audio').html(audio_markup) : $('div#oc_download_audio').html("No audio files present for this media.");
+
+                $('#oc_client_downloads').append("</br><span><b>Please Note :<b></span><br>");
+                $('#oc_client_downloads').append("<span>Some files may not download automatically, you may need to \"Right click\" the download link and select \"Save Link As..\" to save the file. In some browsers where the \"Right click\" is disabled - e.g. Firefox 8 on Mac OS X - you may have to press ALT and then click the download link to prompt a download.</span><br>");
+
+            },
+            error: function(a, b, c){}
+        });
+    };
+
+    /**
+     * @memberOf Opencast.download
+     * @description Synchronous, Domain specific call that returns file size in MB
+    */
+
+    that.getMediaSize = function(url){
+        var req = new XMLHttpRequest();
+        req.open('HEAD', url, false);
+        req.send(null);
+        return Math.round((req.getResponseHeader("Content-Length") / 1048576) * 100) / 100;
+    };
+
+    return that;
+}());
+
+
