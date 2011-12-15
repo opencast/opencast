f4117e1ccafce855991f923db4f4ad6c3b11b79d
msutton <msutton@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-12-15 22:07:56 +0000

    MH-8442, search service can now return a lsit of episodes in a series, and the enage index page can limit to just the series.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@11478 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-search-service-impl/src/main/java/org/opencastproject/search/impl/solr/SolrRequester.java
+++ modules/matterhorn-search-service-impl/src/main/java/org/opencastproject/search/impl/solr/SolrRequester.java
@@ -562,6 +562,19 @@ public class SolrRequester {
       sb.append(")");
     }
 
+    String solrSeriesIdRequest = StringUtils.trimToNull(q.getSeriesId());
+    if (solrSeriesIdRequest != null) {
+      String cleanSolrSeriesIdRequest = SolrUtils.clean(solrSeriesIdRequest);
+      if (sb.length() > 0) {
+        sb.append(" AND ");
+      }
+      sb.append("(");
+      sb.append(Schema.DC_IS_PART_OF);
+      sb.append(":");
+      sb.append(cleanSolrSeriesIdRequest);
+      sb.append(")");
+    }
+    
     String solrTextRequest = StringUtils.trimToNull(q.getText());
     if (solrTextRequest != null) {
       String cleanSolrTextRequest = SolrUtils.clean(q.getText());
