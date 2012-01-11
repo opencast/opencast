9a544b4e8335e806f5224ab2df02e8ce34fcf1c5
rrolf <rrolf@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-01-11 15:42:27 +0000

    MH-8455 merging Markus M.s patches to 1.3 branch
    git-svn-id: https://opencast.jira.com/svn/MH/branches/1.3.x@11574 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/felix/bin/binary_dist_start_matterhorn.sh
+++ docs/felix/bin/binary_dist_start_matterhorn.sh
@@ -17,12 +17,13 @@ fi
 ##
 
 FELIX_OPTS="-Dfelix.home=$FELIX_HOME"
+FELIX_WORK="-Dfelix.work=$FELIX_HOME/work"
 FELIX_FILEINSTALL_OPTS="-Dfelix.fileinstall.dir=$FELIX/load"
 PAX_CONFMAN_OPTS="-Dbundles.configuration.location=$FELIX/conf"
 PAX_LOGGING_OPTS="-Dorg.ops4j.pax.logging.DefaultServiceLog.level=WARN -Dopencast.logdir=$LOGDIR"
 UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX/conf/services/java.util.logging.properties"
 GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
-TEMP_OPTS="-Djava.io.tmpdir=$FELIX_HOME/work"
+TEMP_OTPS="-Djava.io.tmpdir=/tmp"
 MEM="-Xms1024m -Xmx1024m -XX:MaxPermSize=256m"
 
 # Make sure matterhorn bundles are reloaded
@@ -30,4 +31,4 @@ FELIX_CACHE="$FELIX/felix-cache"
 rm -rf $FELIX_CACHE
 
 # Finally start felix
-java $FELIX_OPTS $MEM $GRAPHICS_OPTS $TEMP_OPTS $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS $CXF_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHE
+java $FELIX_OPTS $MEM $GRAPHICS_OPTS $TEMP_OPTS $FELIX_WORK $FELIX_FILEINSTALL_OPTS $PAX_CONFMAN_OPTS $PAX_LOGGING_OPTS $UTIL_LOGGING_OPTS $CXF_OPTS -jar $FELIX/bin/felix.jar $FELIX_CACHE
