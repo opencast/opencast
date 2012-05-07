466ea5c31eb86e1ae6230b0c76488f03dc428fc5
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-05-07 16:29:08 +0000

    Removing accidental commit of a test service, MH-8800
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@12192 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/test-managed-service/src/main/java/org/opencastproject/test/TestService.java
+++
@@ -1,8 +0,0 @@
-package org.opencastproject.test;
-
-/**
- * Service that gets instantiated and registered with the OSGi envirnonment by
- * {@link TestManagedServiceFactory}.
- */
-public class TestService {
-}
