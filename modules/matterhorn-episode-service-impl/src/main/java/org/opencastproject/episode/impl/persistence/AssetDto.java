dee315e97aff917809394b0290631c57c36bd0cb
cedriessen <cedriessen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-07-27 22:16:18 +0000

    MH-8131 -- satisfy checkstyle
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@12574 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-episode-service-impl/src/main/java/org/opencastproject/episode/impl/persistence/AssetDto.java
+++ modules/matterhorn-episode-service-impl/src/main/java/org/opencastproject/episode/impl/persistence/AssetDto.java
@@ -23,7 +23,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
