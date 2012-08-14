0995a004d50c8e4af2202e00b109666e0c647848
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-08-14 19:31:24 +0000

    Merging fix for MH-9000 into 1.4.x
    git-svn-id: https://opencast.jira.com/svn/MH/branches/1.4.x@12717 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-conductor/src/main/java/org/opencastproject/workflow/handler/ComposeWorkflowOperationHandler.java
+++ modules/matterhorn-conductor/src/main/java/org/opencastproject/workflow/handler/ComposeWorkflowOperationHandler.java
@@ -26,6 +26,8 @@ import org.opencastproject.mediapackage.MediaPackageElementFlavor;
 import org.opencastproject.mediapackage.MediaPackageElementParser;
 import org.opencastproject.mediapackage.MediaPackageException;
 import org.opencastproject.mediapackage.Track;
+import org.opencastproject.mediapackage.selector.AbstractMediaPackageElementSelector;
+import org.opencastproject.mediapackage.selector.TrackSelector;
 import org.opencastproject.util.MimeTypes;
 import org.opencastproject.util.NotFoundException;
 import org.opencastproject.workflow.api.AbstractWorkflowOperationHandler;
@@ -41,7 +43,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -58,8 +64,9 @@ public class ComposeWorkflowOperationHandler extends AbstractWorkflowOperationHa
 
   static {
     CONFIG_OPTIONS = new TreeMap<String, String>();
-    CONFIG_OPTIONS.put("source-flavor", "The \"flavor\" of the track to use as a video source input");
-    CONFIG_OPTIONS.put("encoding-profile", "The encoding profile to use");
+    CONFIG_OPTIONS.put("source-flavors", "The \"flavor\" of the track to use as a source input");
+    CONFIG_OPTIONS.put("source-tags", "The \"tag\" of the track to use as a source input");
+    CONFIG_OPTIONS.put("encoding-profiles", "The encoding profile(s) to use");
     CONFIG_OPTIONS.put("target-flavor", "The flavor to apply to the encoded file");
     CONFIG_OPTIONS.put("target-tags", "The tags to apply to the encoded file");
     CONFIG_OPTIONS.put("audio-only", "Set to 'true' to process tracks containing only audio streams");
@@ -106,9 +113,11 @@ public class ComposeWorkflowOperationHandler extends AbstractWorkflowOperationHa
   /**
    * {@inheritDoc}
    * 
-   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#start(org.opencastproject.workflow.api.WorkflowInstance, JobContext)
+   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#start(org.opencastproject.workflow.api.WorkflowInstance,
+   *      JobContext)
    */
-  public WorkflowOperationResult start(final WorkflowInstance workflowInstance, JobContext context) throws WorkflowOperationException {
+  public WorkflowOperationResult start(final WorkflowInstance workflowInstance, JobContext context)
+          throws WorkflowOperationException {
     logger.debug("Running compose workflow operation on workflow {}", workflowInstance.getId());
 
     try {
@@ -138,76 +147,173 @@ public class ComposeWorkflowOperationHandler extends AbstractWorkflowOperationHa
   private WorkflowOperationResult encode(MediaPackage src, WorkflowOperationInstance operation)
           throws EncoderException, IOException, NotFoundException, MediaPackageException, WorkflowOperationException {
     MediaPackage mediaPackage = (MediaPackage) src.clone();
-    // Read the configuration properties
-    String sourceFlavor = StringUtils.trimToNull(operation.getConfiguration("source-flavor"));
-    String encodingProfileName = StringUtils.trimToNull(operation.getConfiguration("encoding-profile"));
-    String audioOnlyConfig = StringUtils.trimToNull(operation.getConfiguration("audio-only"));
-    String videoOnlyConfig = StringUtils.trimToNull(operation.getConfiguration("video-only"));
 
-    if (sourceFlavor == null)
-      throw new IllegalStateException("Source flavor must be specified");
+    // Check which tags have been configured
+    String sourceTagsOption = StringUtils.trimToNull(operation.getConfiguration("source-tags"));
+    String targetTagsOption = StringUtils.trimToNull(operation.getConfiguration("target-tags"));
+    String sourceFlavorOption = StringUtils.trimToNull(operation.getConfiguration("source-flavor"));
+    String sourceFlavorsOption = StringUtils.trimToNull(operation.getConfiguration("source-flavors"));
+    String targetFlavorOption = StringUtils.trimToNull(operation.getConfiguration("target-flavor"));
 
-    // Find the encoding profile
-    EncodingProfile profile = composerService.getProfile(encodingProfileName);
-    if (profile == null) {
-      throw new IllegalStateException("Encoding profile '" + encodingProfileName + "' was not found");
-    }
+    AbstractMediaPackageElementSelector<Track> elementSelector = new TrackSelector();
 
-    // Audio / Video only?
-    boolean audioOnly = audioOnlyConfig != null && Boolean.parseBoolean(audioOnlyConfig);
-    boolean videoOnly = videoOnlyConfig != null && Boolean.parseBoolean(videoOnlyConfig);
-
-    // Depending on the input type of the profile and the configured flavors and
-    // tags, make sure we have the required tracks:
-    Track[] tracks = mediaPackage.getTracks(MediaPackageElementFlavor.parseFlavor(sourceFlavor));
-
-    // Did we get the set of tracks that we need?
-    if (tracks.length == 0) {
-      logger.info("Skipping encoding of media package to '{}': no suitable input tracks found", profile);
+    // Make sure either one of tags or flavors are provided
+    if (StringUtils.isBlank(sourceTagsOption) && StringUtils.isBlank(sourceFlavorOption)
+            && StringUtils.isBlank(sourceFlavorsOption)) {
+      logger.info("No source tags or flavors have been specified, not matching anything");
       return createResult(mediaPackage, Action.CONTINUE);
     }
 
-    // Encode all found tracks
+    // Select the source flavors
+    for (String flavor : asList(sourceFlavorsOption)) {
+      try {
+        elementSelector.addFlavor(MediaPackageElementFlavor.parseFlavor(flavor));
+      } catch (IllegalArgumentException e) {
+        throw new WorkflowOperationException("Source flavor '" + flavor + "' is malformed");
+      }
+    }
+
+    // Support legacy "source-flavor" option
+    if (StringUtils.isNotBlank(sourceFlavorOption)) {
+      String flavor = StringUtils.trim(sourceFlavorOption);
+      try {
+        elementSelector.addFlavor(MediaPackageElementFlavor.parseFlavor(flavor));
+      } catch (IllegalArgumentException e) {
+        throw new WorkflowOperationException("Source flavor '" + flavor + "' is malformed");
+      }
+    }
+
+    // Select the source tags
+    for (String tag : asList(sourceTagsOption)) {
+      elementSelector.addTag(tag);
+    }
+
+    // Find the encoding profile
+    String profilesOption = StringUtils.trimToNull(operation.getConfiguration("encoding-profiles"));
+    List<EncodingProfile> profiles = new ArrayList<EncodingProfile>();
+    for (String profileName : asList(profilesOption)) {
+      EncodingProfile profile = composerService.getProfile(profileName);
+      if (profile == null)
+        throw new WorkflowOperationException("Encoding profile '" + profileName + "' was not found");
+      profiles.add(profile);
+    }
+
+    // Support legacy "encoding-profile" option
+    String profileOption = StringUtils.trimToNull(operation.getConfiguration("encoding-profile"));
+    if (StringUtils.isNotBlank(profileOption)) {
+      String profileId = StringUtils.trim(profileOption);
+      EncodingProfile profile = composerService.getProfile(profileId);
+      if (profile == null)
+        throw new WorkflowOperationException("Encoding profile '" + profileId + "' was not found");
+      profiles.add(profile);
+    }
+
+    // Make sure there is at least one profile
+    if (profiles.isEmpty())
+      throw new WorkflowOperationException("No encoding profile was specified");
+
+    // Audio / Video only?
+    String audioOnlyConfig = StringUtils.trimToNull(operation.getConfiguration("audio-only"));
+    String videoOnlyConfig = StringUtils.trimToNull(operation.getConfiguration("video-only"));
+    boolean audioOnly = audioOnlyConfig != null && Boolean.parseBoolean(audioOnlyConfig);
+    boolean videoOnly = videoOnlyConfig != null && Boolean.parseBoolean(videoOnlyConfig);
+
+    // Target tags
+    List<String> targetTags = asList(targetTagsOption);
+
+    // Target flavor
+    MediaPackageElementFlavor targetFlavor = null;
+    if (StringUtils.isNotBlank(targetFlavorOption)) {
+      try {
+        targetFlavor = MediaPackageElementFlavor.parseFlavor(targetFlavorOption);
+      } catch (IllegalArgumentException e) {
+        throw new WorkflowOperationException("Target flavor '" + targetFlavorOption + "' is malformed");
+      }
+    }
+
+    // Look for elements matching the tag
+    Collection<Track> elements = elementSelector.select(mediaPackage, false);
+
+    // Encode all tracks found
     long totalTimeInQueue = 0;
-    for (Track t : tracks) {
+    Map<Job, JobInformation> encodingJobs = new HashMap<Job, JobInformation>();
+    for (Track track : elements) {
 
       // Skip audio/video only mismatches
-      if (audioOnly && t.hasVideo()) {
-        logger.info("Skipping encoding of '{}', since it contains a video stream", t);
+      if (audioOnly && track.hasVideo()) {
+        logger.info("Skipping encoding of '{}', since it contains a video stream", track);
         continue;
-      } else if (videoOnly && t.hasAudio()) {
-        logger.info("Skipping encoding of '{}', since it containsa an audio stream", t);
-        continue;
-      }
-
-      // Check if the track supports the output type of the profile
-      MediaType outputType = profile.getOutputType();
-      if (outputType.equals(MediaType.Audio) && !t.hasAudio()) {
-        logger.info("Skipping encoding of '{}', since it lacks an audio stream", t);
-        continue;
-      } else if (outputType.equals(MediaType.Visual) && !t.hasVideo()) {
-        logger.info("Skipping encoding of '{}', since it lacks a video stream", t);
+      } else if (videoOnly && track.hasAudio()) {
+        logger.info("Skipping encoding of '{}', since it containsa an audio stream", track);
         continue;
       }
 
-      logger.info("Encoding track {} using encoding profile '{}'", t, profile);
+      // Encode the track with all profiles
+      for (EncodingProfile profile : profiles) {
 
-      // Start encoding and wait for the result
-      Job job = composerService.encode(t, profile.getIdentifier());
-      if (!waitForStatus(job).isSuccess()) {
-        throw new WorkflowOperationException("Encoding failed");
+        // Check if the track supports the output type of the profile
+        MediaType outputType = profile.getOutputType();
+        if (outputType.equals(MediaType.Audio) && !track.hasAudio()) {
+          logger.info("Skipping encoding of '{}', since it lacks an audio stream", track);
+          continue;
+        } else if (outputType.equals(MediaType.Visual) && !track.hasVideo()) {
+          logger.info("Skipping encoding of '{}', since it lacks a video stream", track);
+          continue;
+        }
+
+        logger.info("Encoding track {} using encoding profile '{}'", track, profile);
+
+        // Start encoding and wait for the result
+        encodingJobs.put(composerService.encode(track, profile.getIdentifier()), new JobInformation(track, profile));
       }
-      
-      Track composedTrack = (Track) MediaPackageElementParser.getFromXml(job.getPayload());
+    }
+
+    if (encodingJobs.isEmpty()) {
+      logger.info("No matching tracks found");
+      return createResult(mediaPackage, Action.CONTINUE);
+    }
+
+    // Wait for the jobs to return
+    if (!waitForStatus(encodingJobs.keySet().toArray(new Job[encodingJobs.size()])).isSuccess()) {
+      throw new WorkflowOperationException("One of the encoding jobs did not complete successfully");
+    }
+
+    // Process the result
+    for (Map.Entry<Job, JobInformation> entry : encodingJobs.entrySet()) {
+      Job job = entry.getKey();
+      Track track = entry.getValue().getTrack();
+      EncodingProfile profile = entry.getValue().getProfile();
 
       // add this receipt's queue time to the total
       totalTimeInQueue += job.getQueueTime();
 
-      updateTrackMetadata(composedTrack, operation, profile);
+      Track composedTrack = (Track) MediaPackageElementParser.getFromXml(job.getPayload());
+
+      // Adjust the target tags
+      for (String tag : targetTags) {
+        logger.trace("Tagging composed track with '{}'", tag);
+        composedTrack.addTag(tag);
+      }
+
+      // Adjust the target flavor. Make sure to account for partial updates
+      if (targetFlavor != null) {
+        String flavorType = targetFlavor.getType();
+        String flavorSubtype = targetFlavor.getSubtype();
+        if ("*".equals(flavorType))
+          flavorType = track.getFlavor().getType();
+        if ("*".equals(flavorSubtype))
+          flavorSubtype = track.getFlavor().getSubtype();
+        composedTrack.setFlavor(new MediaPackageElementFlavor(flavorType, flavorSubtype));
+        logger.debug("Composed track has flavor '{}'", composedTrack.getFlavor());
+      }
+
+      // Set the mimetype
+      if (profile.getMimeType() != null)
+        composedTrack.setMimeType(MimeTypes.parseMimeType(profile.getMimeType()));
 
       // store new tracks to mediaPackage
-      mediaPackage.addDerived(composedTrack, t);
-      String fileName = getFileNameFromElements(t, composedTrack);
+      mediaPackage.addDerived(composedTrack, track);
+      String fileName = getFileNameFromElements(track, composedTrack);
       composedTrack.setURI(workspace.moveTo(composedTrack.getURI(), mediaPackage.getIdentifier().toString(),
               composedTrack.getIdentifier(), fileName));
     }
@@ -217,31 +323,37 @@ public class ComposeWorkflowOperationHandler extends AbstractWorkflowOperationHa
     return result;
   }
 
-  // Update the newly composed track with metadata
-  private void updateTrackMetadata(Track composedTrack, WorkflowOperationInstance operation, EncodingProfile profile) {
-    // Read the configuration properties
-    String targetTrackTags = StringUtils.trimToNull(operation.getConfiguration("target-tags"));
-    String targetTrackFlavor = StringUtils.trimToNull(operation.getConfiguration("target-flavor"));
+  /**
+   * This class is used to store context information for the jobs.
+   */
+  private static final class JobInformation {
 
-    if (composedTrack == null)
-      throw new IllegalStateException("unable to retrieve composed track");
+    private Track track = null;
+    private EncodingProfile profile = null;
 
-    // Add the flavor, either from the operation configuration or from the
-    // composer
-    if (targetTrackFlavor != null)
-      composedTrack.setFlavor(MediaPackageElementFlavor.parseFlavor(targetTrackFlavor));
-    logger.debug("Composed track has flavor '{}'", composedTrack.getFlavor());
-
-    // Set the mimetype
-    if (profile.getMimeType() != null)
-      composedTrack.setMimeType(MimeTypes.parseMimeType(profile.getMimeType()));
-
-    // Add tags
-    List<String> targetTags = asList(targetTrackTags);
-    for (String tag : targetTags) {
-      logger.trace("Tagging composed track with '{}'", tag);
-      composedTrack.addTag(tag);
+    JobInformation(Track track, EncodingProfile profile) {
+      this.track = track;
+      this.profile = profile;
     }
+
+    /**
+     * Returns the track.
+     * 
+     * @return the track
+     */
+    public Track getTrack() {
+      return track;
+    }
+
+    /**
+     * Returns the profile.
+     * 
+     * @return the profile
+     */
+    public EncodingProfile getProfile() {
+      return profile;
+    }
+
   }
 
 }
