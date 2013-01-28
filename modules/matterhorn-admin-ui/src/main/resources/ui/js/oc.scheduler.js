3bb72e343940758362e2d05ed5c23fde6d309b2f
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-01-28 20:19:17 +0000

    Merging new fix for MH-9309 into 1.4.x
    git-svn-id: https://opencast.jira.com/svn/MH/branches/1.4.x@13752 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.scheduler.js
+++ modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.scheduler.js
@@ -68,7 +68,7 @@ var ocScheduler = (function() {
     this.registerCatalogs();
     this.registerEventHandlers();
 
-    ocWorkflow.init($('#workflowSelector'), $('#workflowConfigContainer'));
+    ocWorkflow.init($('#workflowSelector'), $('#workflowConfigContainer'), ['schedule']);
 
     if(this.type === SINGLE_EVENT){
       this.agentList = '#agent';
