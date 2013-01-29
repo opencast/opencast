dde24fd209f7ab9d970aada63b4f227ccb17b4a9
lrohner <lrohner@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-01-29 02:30:43 +0000

    MH-9403 -- Fixed unnecessary capture agent update
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@13763 e6edf6fb-f266-4316-afb4-e53d95876a76

--- modules/matterhorn-capture-admin-service-impl/src/main/java/org/opencastproject/capture/admin/impl/CaptureAgentStateServiceImpl.java
+++ modules/matterhorn-capture-admin-service-impl/src/main/java/org/opencastproject/capture/admin/impl/CaptureAgentStateServiceImpl.java
@@ -48,10 +48,10 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.persistence.EntityManager;
@@ -247,7 +247,7 @@ public class CaptureAgentStateServiceImpl implements CaptureAgentStateService, M
       Organization org = securityService.getOrganization();
       AgentImpl a = new AgentImpl(agentName, org.getId(), state, "", new Properties());
       updateAgentInDatabase(a);
-    } else {
+    } else if (!agent.getState().equals(state)) {
       // the agent is known, so set the state
       logger.debug("Setting Agent {} to state {}.", agentName, state);
       agent.setState(state);
