543ceb83060d6de49dea5b7dd5e87402f705467f
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-07-31 19:16:15 +0000

    Adding conversion script to handle changes made by MH-9798.  Needs testing\!
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@15207 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ docs/upgrade/1.4.0_to_1.4.1/postgres84.sql
@@ -0,0 +1,9 @@
+DROP INDEX "IX_mh_annotation_user" ON "mh_annotation";
+DROP INDEX "IX_mh_user_action_user" ON "mh_user_action";
+
+ALTER TABLE mh_annotation CHANGE user user_id VARCHAR(255);
+ALTER TABLE mh_user_action CHANGE user user_id VARCHAR(255);
+
+CREATE INDEX "IX_mh_annotation_user_id" ON "mh_annotation" ("user_id");
+CREATE INDEX "IX_mh_user_action_user_id" ON "mh_user_action" ("user_id");
+
