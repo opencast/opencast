55822f38ed4127f4529a5b5fa4f8c402a2c76255
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-08-15 19:54:34 +0000

    Adding code to handle 1.4.x -> 1.4.1 upgrades (at least so far) as part of MH-9473.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@15363 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/upgrade/1.4.0_to_1.4.1/mysql5.sql
+++ docs/upgrade/1.4.0_to_1.4.1/mysql5.sql
@@ -1,6 +1,14 @@
 DROP INDEX IX_mh_annotation_user ON mh_annotation;
-DROP INDEX IX_mh_user_action_user ON mh_user_action; -- Works for 1.3.x upgrade path
-DROP INDEX IX_mh_user_action_user_id ON mh_user_action; -- Works for 1.4.0 ddl
+
+set @exist := (select count(*) from information_schema.statistics where table_name = 'mh_user_action' and index_name = 'IX_mh_user_action_user');
+set @sqlstmt := if( @exist > 0, 'DROP INDEX IX_mh_user_action_user ON mh_user_action', 'SELECT "1.4.x schema detected"' );
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
+
+set @exist := (select count(*) from information_schema.statistics where table_name = 'mh_user_action' and index_name = 'IX_mh_user_action_user_id');
+set @sqlstmt := if( @exist > 0, 'DROP INDEX IX_mh_user_action_user_id ON mh_user_action', 'SELECT "1.3.x upgraded schema detected"' );
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
 
 ALTER TABLE mh_annotation CHANGE user user_id VARCHAR(255);
 ALTER TABLE mh_user_action CHANGE user user_id VARCHAR(255);
