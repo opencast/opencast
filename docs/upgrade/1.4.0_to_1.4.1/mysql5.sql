8e9a47274e6e8daf5599fd6c86bdf8b2f5282b93
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-09-25 15:31:32 +0000

    Merging fix for MH-9901 into 1.4.x.  Jobs in process when a core shuts down will now be cleaned up properly.
    git-svn-id: https://opencast.jira.com/svn/MH/branches/1.4.x@15806 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/upgrade/1.4.0_to_1.4.1/mysql5.sql
+++ docs/upgrade/1.4.0_to_1.4.1/mysql5.sql
@@ -16,3 +16,7 @@ ALTER TABLE mh_user_action CHANGE user user_id VARCHAR(255);
 CREATE INDEX IX_mh_annotation_user_id ON mh_annotation (user_id);
 CREATE INDEX IX_mh_user_action_user_d ON mh_user_action (user_id);
 
+set @exist := (select count(*) from information_schema.statistics where TABLE_NAME = "mh_job_argument" and INDEX_NAME = "UNQ_job_argument_0");
+set @sqlstmt := if( @exist > 0, 'DROP INDEX UNQ_job_argument_0 on mh_job_argument;', 'SELECT "1.3.x upgraded schema detected"' );
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
