f0c09827d78a09233083c5e6a87d8458a284da0a
greg_logan <greg_logan@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-09-27 17:07:44 +0000

    Committing fix for the upgrade scripts as part of MH-9828.  Drops a unique constraint.
    git-svn-id: https://opencast.jira.com/svn/MH/trunk@15821 e6edf6fb-f266-4316-afb4-e53d95876a76

--- docs/upgrade/1.4.0_to_1.4.1/postgres84.sql
+++ docs/upgrade/1.4.0_to_1.4.1/postgres84.sql
@@ -7,3 +7,4 @@ ALTER TABLE mh_user_action CHANGE user user_id VARCHAR(255);
 CREATE INDEX "IX_mh_annotation_user_id" ON "mh_annotation" ("user_id");
 CREATE INDEX "IX_mh_user_action_user_id" ON "mh_user_action" ("user_id");
 
+DROP INDEX IF EXISTS "UNQ_job_argument_0" ON "mh_job_argument";
