bd9e097197c30ddf64b9ffd4b1ce46707b047363
Greg Logan <greg.logan@usask.ca>
2013-10-30 15:58:17 -0600

    Initial rev of the tag script.  Need to handle RC vs full releases before it's ready.  Part of MH-9948.

--- docs/scripts/release/tag.sh
+++ docs/scripts/release/tag.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#The comments in this file assume you are creating a tag from a branch
-#Eg:  The defaults in the comments here will take the current version of 
-#https://opencast.jira.com/svn/MH/branches/1.3.x and tag it as 
-#https://opencast.jira.com/svn/MH/tags/1.3-rc5
+. ${functions.sh}
 
-#The name of the branch in SVN that we are looking to turn into a release
-#E.g. BRANCH_NAME=1.3.x
-BRANCH_NAME=
+#TODO: Handle release vs RC destinction (one uses git flow, one does not...)
+
+#The comments in this file assume you are creating a tag from a release branch
+#This script does *not* push any changes, so it should be safe to experiment with locally
 
 #The version the POMs are in the development branch.
 #E.g. BRANCH_POM_VER=1.3-SNAPSHOT
@@ -20,35 +18,21 @@ RELEASE_VER=
 #The jira ticket this work is being done under (must be open)
 JIRA_TICKET=
 
-#The scratch directory where the work is performed.  Make sure you have enough
-#space.  Should not already include a subdirectory of $WORK_DIR/$JIRA_TICKET
-WORK_DIR=/tmp/
-
 #=======You should not need to modify anything below this line=================
+
+WORK_DIR=$(echo `pwd` | sed 's/\(.*\/.*\)\/docs\/scripts\/release/\1/g')
+
+#Get the current branch name
+curBranch=`git status | grep "On branch" | sed 's/\# On branch \(.*\)/\1/'`
+
+echo "Warning, this script will create a mini branch off of $curBranch to do the POM version modification"
+yesno -d "n" "Do you wish to continue" cont
+if [[ ! "$ok" ]]; then
+  return 1
+fi
 
 #The version we want the poms to be, usually the same as RELEASE_VER
 TAG_POM_VER=$RELEASE_VER
-
-#The actual working dir
-WORK_DIR=$WORK_DIR/$JIRA_TICKET
-
-#Matterhorn base URL
-SVN_URL=https://opencast.jira.com/svn/MH
-
-BRANCH_URL=$SVN_URL/branches/$BRANCH_NAME
-TAG_URL=$SVN_URL/tags/$RELEASE_VER
-
-#TODO: We should use an svn switch instead because while we are working on this
-#tag to get it ready people might think it has been released.
-echo "Creating new tag by copying $BRANCH_URL to $TAG_URL."
-svn copy $BRANCH_URL $TAG_URL -m "$JIRA_TICKET Creating $TAG_NAME Tag"
-
-echo "Creating scratch dir and checking out release sources"
-pushd .
-rm -rf $WORK_DIR
-mkdir $WORK_DIR
-cd $WORK_DIR
-svn co $TAG_URL .
 
 echo "Replacing POM file version in main POM."
 sed -i "s/<version>$BRANCH_POM_VER/<version>$TAG_POM_VER/" $WORK_DIR/pom.xml
@@ -61,8 +45,8 @@ do
         sleep 1
     fi
 done
-svn commit -m "$JIRA_TICKET Updated pom.xml files to reflect correct version.  Done via docs/scripts/release/tag.sh"
+git checkout -b r/$JIRA_TICKET-$RELEASE_VER
+git commit -a -m "Creating $RELEASE_VER branch to contain POM changes and tag"
+git tag -s $RELEASE_VER -m "Creating $RELEASE_VER branch and tag as part of $JIRA_TICKET"
 
-#Return to previous environment and cleanup
-popd
-rm -rf $WORK_DIR
+echo "Please verify that things look correct, and then push the new branch and tag upstream!"
