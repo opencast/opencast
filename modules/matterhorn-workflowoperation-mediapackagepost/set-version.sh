5358b6b38235107a409985544412ad5477fb20c2
Lars Kiesow <lkiesow@uos.de>
2013-11-26 11:53:17 +0100

    MH-9989, cleanup

--- modules/matterhorn-workflowoperation-mediapackagepost/set-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]
-then
-	echo "Usage: $0 version"
-	exit
-fi
-
-sed -i "s/<version>MH_VERSION<\/version>/<version>$1<\/version>/" modules/matterhorn-workflowoperation-mediapackagepost/pom.xml
