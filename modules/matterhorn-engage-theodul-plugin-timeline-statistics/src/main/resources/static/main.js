83af1e54b6969ce618222763b604e63c1e3f1e42
Martin Abel <martin@Schleppi.(none)>
2014-01-06 19:06:36 +0100

    MH-9971 add highcharts lib, add stat chart, fix some view problems, prepair footprint data

--- modules/matterhorn-engage-theodul-plugin-timeline-statistics/src/main/resources/static/main.js
+++ modules/matterhorn-engage-theodul-plugin-timeline-statistics/src/main/resources/static/main.js
@@ -29,31 +29,110 @@ define(['require', 'jquery', 'underscore', 'backbone', 'engage/engage_core'], fu
       template: PLUGIN_TEMPLATE
   };
   
-  var initCount = 2; //wait for two inits, plugin load done and mediapackage
+  var initCount = 6; //wait for 4 inits, plugin load done, mediapackage, 2 highchart libs
   
   var StatisticsTimelineView = Backbone.View.extend({
-    initialize: function(mediaPackageModel, template) {
+    initialize: function() {
         this.setElement($(plugin.container)); // Every plugin view has it's own container associated with it
-        this.model = mediaPackageModel;
-        this.template = template;
+        this.videoData = Engage.model.get("videoDataModel");
+        this.footprints = Engage.model.get("footprints");
+        this.template = plugin.template;
         //bound the render function always to the view
         _.bindAll(this, "render");
         //listen for changes of the model and bind the render function to this
-        this.model.bind("change", this.render);
+        this.videoData.bind("change", this.render);
+        this.footprints.bind("change", this.render);
+        this.render();
     },
     render: function() {
         //format values
         var tempVars = {
-            test: "Statistics"
+            width: "1329",
+            height: "70"
         };
         // compile template and load into the html
         this.$el.html(_.template(this.template, tempVars));
+        
+        var duration = this.videoData.get("duration");
+        //canvas insert done, init highchart js
+        
+        //fill array 
+        var data = new Array();
+        var cView = 0;
+        for(i=0;i<duration/1000;i++){
+          _.each(this.footprints, function(element, index, list){
+            if(this.footprints.at(index).get("position") == i)
+              cView = this.footprints.at(index).get("views");
+          }, this);
+          data.push(cView);
+        }
+
+        //init highchart with options to the div container
+        $('#engage_timeline_statistics_chart').highcharts({
+          title : {
+            text : ""
+          },
+          credits : {
+            enabled : false
+          },
+          exporting : {
+            enabled : false
+          },
+          chart : {
+            type : 'areaspline',
+            spacing : [ 10, 10, 10, 0 ],
+            zoomType: 'x'
+          },
+          tooltip : {
+            formatter : function () {
+              var duration = moment.duration(this.x, "seconds");
+              var tooltip = '<b>' + this.y + ' Views at</b><br/>' + duration.hours() + ':' + duration.minutes() + ':' + duration.seconds();
+              return tooltip;
+            }
+          },
+          yAxis : {
+            title : {
+              enabled : false
+            },
+            labels : {
+              enabled : false
+            }
+          },
+          xAxis : {
+            title : {
+              align : "low",
+              text : "Minutes"
+            },
+            labels : {
+              formatter : function () {
+                return Math.round(this.value / 60)
+              }
+            }
+          },
+          plotOptions : {
+            areaspline : {
+              marker : {
+                enabled : false
+              }
+            }
+          },
+          legend : {
+            enabled : false
+          },
+          series : [ {
+            name : "Views",
+            data : data
+          } ]
+        });    
     }
   });
 
   function initPlugin() {
+    Engage.log("Timeline: Statistics: init view");
     //create a new view with the media package model and the template
-    new StatisticsTimelineView(Engage.model.get("mediaPackage"), plugin.template);
+    //new StatisticsTimelineView(Engage.model.get("mediaPackage"), plugin.template);
+    //new StatisticsTimelineView(Engage.model.get("videoDataModel"), plugin.template);
+    new StatisticsTimelineView("");
   }
   
 	//Init Event
@@ -66,6 +145,46 @@ define(['require', 'jquery', 'underscore', 'backbone', 'engage/engage_core'], fu
     }
   });
   
+  Engage.model.on("change:footprints", function() { 
+    initCount -= 1;
+    if (initCount === 0) {
+        initPlugin();
+    }
+  });
+  
+  Engage.model.on("change:videoDataModel", function() {
+    initCount -= 1;
+    if (initCount === 0) {
+        initPlugin();
+    }      
+  });
+  
+  // load highchart lib
+  require(["./lib/highchart/highcharts.js"], function(videojs) {
+      Engage.log("Statistics Timeline: Load highcharts.js done");
+      initCount -= 1;
+      if (initCount === 0) {
+          initPlugin();
+      }
+  });
+  /*
+  // load highchart exporting module lib
+  require(["./lib/highchart/modules/exporting.js"], function(videojs) {
+      Engage.log("Statistics Timeline: Load highchart exporting module done");
+      initCount -= 1;
+      if (initCount === 0) {
+          initPlugin();
+      }
+  });
+  */
+  // Load moment.js lib
+  require(["./lib/moment.min.js"], function(momentjs) {
+      Engage.log("Description: load moment.min.js done");
+      initCount -= 1;
+      if (initCount === 0) {
+          initPlugin();
+      }
+  });
   //All plugins loaded lets do some stuff
   Engage.on("Core:plugin_load_done", function() {   	
     initCount -= 1;
