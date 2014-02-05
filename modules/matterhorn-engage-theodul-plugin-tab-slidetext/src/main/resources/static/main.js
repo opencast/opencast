c3f43ebf7378a7d4713e14baf56f1c4fb20b438d
Dennis Boeckmann <dev@dbck.de>
2014-02-05 13:35:08 +0100

    Reindent and fixing JSLint error warnings.

--- modules/matterhorn-engage-theodul-plugin-tab-slidetext/src/main/resources/static/main.js
+++ modules/matterhorn-engage-theodul-plugin-tab-slidetext/src/main/resources/static/main.js
@@ -16,35 +16,35 @@
 /*jslint browser: true, nomen: true*/
 /*global define*/
 define(['require', 'jquery', 'underscore', 'backbone', 'engage/engage_core'], function (require, $, _, Backbone, Engage) {
-    //
-    "use strict"; // strict mode in all our application
-    //
-	var PLUGIN_NAME = "Slide Text";
-	var PLUGIN_TYPE = "engage_tab";
-	var PLUGIN_VERSION = "0.1";
-	var PLUGIN_TEMPLATE = "template.html";
-	var PLUGIN_STYLES = ["style.css"];
-	
-	//Init Event
-    Engage.log("Tab:Slidetext: init");
-    var relative_plugin_path = Engage.getPluginPath('EngagePluginTabSlidetext');
-    Engage.log('TabSlidetext: relative plugin path ' + relative_plugin_path);
-    //Load other needed JS stuff with Require
-    //require(["./js/bootstrap/js/bootstrap.js"]);
-    //require(["./js/jqueryui/jquery-ui.min.js"]);	
-    
-    //All plugins loaded lets do some stuff
-    Engage.on("Core:plugin_load_done", function() {
-    	
-    	Engage.log("Tab:Slidetext: receive plugin load done");
-	    
-    });
-   
-    return {
-		name: PLUGIN_NAME,
-		type: PLUGIN_TYPE,
-		version: PLUGIN_VERSION,
-		styles: PLUGIN_STYLES,
-		template: PLUGIN_TEMPLATE
-	}
+  //
+  "use strict"; // strict mode in all our application
+  //
+  var PLUGIN_NAME = "Slide Text";
+  var PLUGIN_TYPE = "engage_tab";
+  var PLUGIN_VERSION = "0.1";
+  var PLUGIN_TEMPLATE = "template.html";
+  var PLUGIN_STYLES = ["style.css"];
+
+  //Init Event
+  Engage.log("Tab:Slidetext: init");
+  var relative_plugin_path = Engage.getPluginPath('EngagePluginTabSlidetext');
+  Engage.log('TabSlidetext: relative plugin path ' + relative_plugin_path);
+  //Load other needed JS stuff with Require
+  //require(["./js/bootstrap/js/bootstrap.js"]);
+  //require(["./js/jqueryui/jquery-ui.min.js"]);
+
+  //All plugins loaded lets do some stuff
+  Engage.on("Core:plugin_load_done", function() {
+
+    Engage.log("Tab:Slidetext: receive plugin load done");
+
+  });
+
+  return {
+    name: PLUGIN_NAME,
+    type: PLUGIN_TYPE,
+    version: PLUGIN_VERSION,
+    styles: PLUGIN_STYLES,
+    template: PLUGIN_TEMPLATE
+  };
 });
