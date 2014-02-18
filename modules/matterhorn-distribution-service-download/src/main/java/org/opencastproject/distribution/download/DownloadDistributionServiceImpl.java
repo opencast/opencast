0436ae9ee4804b88449923a1d85663e3839bcad8
Lars Kiesow <lkiesow@uos.de>
2014-02-18 17:43:43 +0100

    MH-9936, Fixed style errors
    This just fixes a couple of style issues.
    
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- modules/matterhorn-distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
+++ modules/matterhorn-distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
@@ -116,7 +116,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Activate method for this OSGi service implementation.
-   * 
+   *
    * @param cc
    *          the OSGi component context
    */
@@ -157,7 +157,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Distribute a Mediapackage element to the download distribution service.
-   * 
+   *
    * @param mediapackage
    *          The media package that contains the element to distribute.
    * @param elementId
@@ -221,29 +221,29 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
               channelId));
       URI uri = distributedElement.getURI();
       long now = 0L;
-      
+
       // Start itbwpdk
       // If the distribution channel is engage player
       // and the file is available locally
       // do check on file level for existence
       if ("engage-player".equals(channelId) && distributionDirectory.exists()) {
-      
-	  File xelement = null;
-	  String buildpath = "";
-	  boolean calc = false;
-	  for (String t : uri.toString().split("/")) {
-			if (calc) {
-			buildpath = buildpath + "/" + t;
-			}
-			if ("static".equals(t)) {
-				calc = true;
-			}
-	  }
-	  xelement = new File(distributionDirectory.getPath().concat(buildpath));	      
+
+    File xelement = null;
+    String buildpath = "";
+    boolean calc = false;
+    for (String t : uri.toString().split("/")) {
+      if (calc) {
+      buildpath = buildpath + "/" + t;
+      }
+      if ("static".equals(t)) {
+        calc = true;
+      }
+    }
+    xelement = new File(distributionDirectory.getPath().concat(buildpath));
       while (checkAvailability) {
 
       if (xelement.exists()) {
-    	  logger.debug("Distributed file was created in download directory for engage player, " + xelement.getPath());  
+        logger.debug("Distributed file was created in download directory for engage player, " + xelement.getPath());
         break;
       }
       if (now < TIMEOUT) {
@@ -258,10 +258,10 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
       logger.warn("Distributed file not created in download directory for engage player, " + xelement.getPath());
       throw new DistributionException("Distributed file not created, " + xelement.getPath());
       }
-      
+
       } else {
-      
-      
+
+
         while (checkAvailability) {
         HttpResponse response = trustedHttpClient.execute(new HttpHead(uri));
         if (response.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK)
@@ -279,10 +279,10 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
         logger.warn("Status code of distributed file {}: {}", uri, response.getStatusLine().getStatusCode());
         throw new DistributionException("Unable to load distributed file " + uri.toString());
       }
-      
+
       }
-      
-      
+
+
       return distributedElement;
     } catch (Exception e) {
       logger.warn("Error distributing " + element, e);
@@ -311,7 +311,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
    * Retract a media package element from the distribution channel. The retracted element must not necessarily be the
    * one given as parameter <code>elementId</code>. Instead, the element's distribution URI will be calculated. This way
    * you are able to retract elements by providing the "original" element here.
-   * 
+   *
    * @param channelId
    *          the channel id
    * @param mediapackage
@@ -370,7 +370,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * @see org.opencastproject.job.api.AbstractJobProducer#process(org.opencastproject.job.api.Job)
    */
   @Override
@@ -406,7 +406,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Gets the destination file to copy the contents of a mediapackage element.
-   * 
+   *
    * @return The file to copy the content to
    */
   protected File getDistributionFile(String channelId, MediaPackage mp, MediaPackageElement element) {
@@ -429,7 +429,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Gets the directory containing the distributed files for this mediapackage.
-   * 
+   *
    * @return the filesystem directory
    */
   protected File getMediaPackageDirectory(String channelId, MediaPackage mp) {
@@ -438,7 +438,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Gets the URI for the element to be distributed.
-   * 
+   *
    * @param mediaPackageId
    *          the mediapackage identifier
    * @param element
@@ -457,7 +457,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Callback for the OSGi environment to set the workspace reference.
-   * 
+   *
    * @param workspace
    *          the workspace
    */
@@ -467,7 +467,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Callback for the OSGi environment to set the service registry reference.
-   * 
+   *
    * @param serviceRegistry
    *          the service registry
    */
@@ -477,7 +477,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * @see org.opencastproject.job.api.AbstractJobProducer#getServiceRegistry()
    */
   @Override
@@ -487,7 +487,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Callback for setting the security service.
-   * 
+   *
    * @param securityService
    *          the securityService to set
    */
@@ -497,7 +497,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Callback for setting the trusted HTTP client.
-   * 
+   *
    * @param trustedHttpClient
    *          the trusted HTTP client to set
    */
@@ -507,7 +507,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Callback for setting the user directory service.
-   * 
+   *
    * @param userDirectoryService
    *          the userDirectoryService to set
    */
@@ -517,7 +517,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * Sets a reference to the organization directory service.
-   * 
+   *
    * @param organizationDirectory
    *          the organization directory
    */
@@ -527,7 +527,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * @see org.opencastproject.job.api.AbstractJobProducer#getSecurityService()
    */
   @Override
@@ -537,7 +537,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * @see org.opencastproject.job.api.AbstractJobProducer#getUserDirectoryService()
    */
   @Override
@@ -547,7 +547,7 @@ public class DownloadDistributionServiceImpl extends AbstractJobProducer impleme
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * @see org.opencastproject.job.api.AbstractJobProducer#getOrganizationDirectoryService()
    */
   @Override
