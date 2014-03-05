664075395ce57ce8b0009a2adb70c4f5773064c7
Lars Kiesow <lkiesow@uos.de>
2014-03-05 17:58:50 +0100

    MH-10166, Check for Success State when Probing for Tesseract
    The TesseractTextExtractorTest will probe for tesseract by checking that
    it return an error code when called with incorrect parameters. This
    means that the test will still be executed if something is horribly
    wrong with your tesseract.
    
    This commit will fix this behavior by checking for a success code
    instead.
    
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- modules/matterhorn-textextractor-tesseract/src/test/java/org/opencastproject/analysis/text/TesseractTextExtractorTest.java
+++ modules/matterhorn-textextractor-tesseract/src/test/java/org/opencastproject/analysis/text/TesseractTextExtractorTest.java
@@ -70,13 +70,14 @@ public class TesseractTextExtractorTest {
     StringBuffer errorBuffer = new StringBuffer();
     Process p = null;
     try {
-      p = new ProcessBuilder(tesseractbinary).start();
+      String[] command = {tesseractbinary, "-v"};
+      p = new ProcessBuilder(command).start();
       stdout = new StreamHelper(p.getInputStream());
       stderr = new StreamHelper(p.getErrorStream(), errorBuffer);
       int status = p.waitFor();
       stdout.stopReading();
       stderr.stopReading();
-      if (status != 1)
+      if (status != 0)
         throw new IllegalStateException();
     } catch (Throwable t) {
       logger.warn("Skipping text analysis tests due to unsatisifed tesseract installation");
