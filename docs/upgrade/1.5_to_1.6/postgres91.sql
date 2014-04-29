9196389f7f013a722a0397dc6e6b1567db670b12
Lukas Rohner <lukas@entwinemedia.com>
2014-04-29 14:11:00 +0200

    MH-9990 -- Added missing ddl scripts for workflow cleanup strategy

---
+++ docs/upgrade/1.5_to_1.6/postgres91.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "mh_job_mh_service_registration" (
+  "Job_id" bigint NOT NULL,
+  "servicesRegistration_id" bigint NOT NULL,
+  PRIMARY KEY ("Job_id", "servicesRegistration_id"),
+  KEY "mhjobmhservice_registrationservicesRegistration_id" ("servicesRegistration_id"),
+  CONSTRAINT "FK_mh_job_mh_service_registration_Job_id" FOREIGN KEY ("Job_id") REFERENCES "mh_job" ("id") ON DELETE CASCADE,
+  CONSTRAINT "mhjobmhservice_registrationservicesRegistration_id" FOREIGN KEY ("servicesRegistration_id") REFERENCES "mh_service_registration" ("id") ON DELETE CASCADE
+);
