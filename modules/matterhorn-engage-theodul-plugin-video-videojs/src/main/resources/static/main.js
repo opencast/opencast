8b9d9be2a6a6e0f00d59082a60fac1eb45876ba7
Denis Meyer <calltopower88@gmail.com>
2014-07-11 11:54:41 +0200

    MH-10354 fixed an event bug

--- modules/matterhorn-engage-theodul-plugin-video-videojs/src/main/resources/static/main.js
+++ modules/matterhorn-engage-theodul-plugin-video-videojs/src/main/resources/static/main.js
@@ -1,4 +1,4 @@
-3/**
+/**
  * Copyright 2009-2011 The Regents of the University of California Licensed
  * under the Educational Community License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain a
