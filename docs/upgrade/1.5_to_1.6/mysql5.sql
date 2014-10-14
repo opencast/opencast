e7ab71c0d156aa13bc74249242aefd13ee280560
Greg Logan <gregorydlogan@gmail.com>
2014-10-14 10:42:17 -0600

    MH-10419: Merged develop in to keep things up to date, updated DDL upgrade scripts.

--- docs/upgrade/1.5_to_1.6/mysql5.sql
+++ docs/upgrade/1.5_to_1.6/mysql5.sql
@@ -27,26 +27,3 @@ CREATE TABLE mh_incident_text (
   text VARCHAR(2038) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE mh_user_session (
-  session_id VARCHAR(50) NOT NULL,
-  user_ip VARCHAR(255),
-  user_agent VARCHAR(255),
-  user_id VARCHAR(255),
-  PRIMARY KEY (session_id)
-) ENGINE=InnoDB;
-
-#Copy over the relevant session data
-INSERT INTO mh_user_session (session_id, user_ip, user_id) SELECT session, user_id, user_ip FROM mh_user_action GROUP BY session;
-
-ALTER TABLE mh_user_action CHANGE session session_id VARCHAR(50);
-
-DROP INDEX IX_mh_user_action_user_id ON mh_user_action;
-DROP INDEX IX_mh_user_action_session_id ON mh_user_action;
-
-ALTER TABLE mh_user_action DROP COLUMN user_id;
-ALTER TABLE mh_user_action DROP COLUMN user_ip;
-
-ALTER TABLE mh_user_action ADD CONSTRAINT FK_mh_user_action_session_id FOREIGN KEY (session_id) REFERENCES mh_user_session (session_id);
-
-CREATE INDEX IX_mh_user_session_user_id ON mh_user_session (user_id);
