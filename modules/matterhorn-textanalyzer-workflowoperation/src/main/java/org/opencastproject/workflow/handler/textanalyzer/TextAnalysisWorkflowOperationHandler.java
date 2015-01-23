4838dcfe25648e45d3759cb11c030638134caaf9
Michael Stypa <michael@stypa.info>
2015-01-23 15:58:55 +0100

    MH-10510 conductor-split: fixed namespaces and added pom to textanalyzer workflowoperation

--- modules/matterhorn-textanalyzer-workflowoperation/src/main/java/org/opencastproject/workflow/handler/textanalyzer/TextAnalysisWorkflowOperationHandler.java
+++ modules/matterhorn-textanalyzer-workflowoperation/src/main/java/org/opencastproject/workflow/handler/textanalyzer/TextAnalysisWorkflowOperationHandler.java
@@ -13,7 +13,7 @@
  *  permissions and limitations under the License.
  *
  */
-package org.opencastproject.workflow.handler;
+package org.opencastproject.workflow.handler.textanalyzer;
 
 import org.opencastproject.composer.api.ComposerService;
 import org.opencastproject.composer.api.EncoderException;
