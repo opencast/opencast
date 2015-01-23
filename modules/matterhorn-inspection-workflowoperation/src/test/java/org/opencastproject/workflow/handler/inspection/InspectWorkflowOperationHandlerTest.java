2f2cbfa27b6621ad83c2295a3d195432eab86952
Michael Stypa <michael@stypa.info>
2015-01-23 14:24:54 +0100

    MH-10510 conductor-split: fixed namespaces and added pom to inspection workflowoperation

--- modules/matterhorn-inspection-workflowoperation/src/test/java/org/opencastproject/workflow/handler/inspection/InspectWorkflowOperationHandlerTest.java
+++ modules/matterhorn-inspection-workflowoperation/src/test/java/org/opencastproject/workflow/handler/inspection/InspectWorkflowOperationHandlerTest.java
@@ -13,7 +13,7 @@
  *  permissions and limitations under the License.
  *
  */
-package org.opencastproject.workflow.handler;
+package org.opencastproject.workflow.handler.inspection;
 
 import org.opencastproject.inspection.api.MediaInspectionService;
 import org.opencastproject.job.api.Job;
