0393af52592be0ba6f0102fbd47b2f86c6a654fd
Ruben Perez Vazquez <ruben.perez@uni-koeln.de>
2016-01-20 17:44:01 +0100

    MH-8577 : Improve the mapping between LDAP attributes and Matterhorn roles
    * Improvement: The attribute-based authorities populator accepts a list of
                   attributes where to look for roles, instead of a single one.
    * Fix:         Handle the possible NullPointerException when one of the
                   listed roles does not exist on the user.
    
    Conflicts:
    	etc/security/security_sample_ldap_xml

--- modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/userdirectory/LdapAttributeAuthoritiesPopulator.java
+++ modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/userdirectory/LdapAttributeAuthoritiesPopulator.java
@@ -33,7 +33,7 @@ import java.util.Set;
 
 public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulator {
 
-  private String roleAttribute;
+  private String[] attributeNames;
   private static final Logger logger = LoggerFactory.getLogger(LdapAttributeAuthoritiesPopulator.class);
 
   /**
@@ -42,33 +42,42 @@ public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulat
    * This assumes that the authenticated users contain an attribute consisting of a space- or comma-separated list of
    * authorities
    *
-   * @param roleAttribute
+   * @param attributeNames
    *          The LDAP user attribute containing the roles
    */
-  public LdapAttributeAuthoritiesPopulator(String roleAttribute) {
-    this.roleAttribute = roleAttribute;
+  public LdapAttributeAuthoritiesPopulator(String... attributeNames) {
+    this.attributeNames = attributeNames;
   }
 
   @Override
   public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations userData, String username) {
     Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-    try {
-      for (String attribute : userData.getStringAttributes(roleAttribute)) {
-        for (String authority : attribute.split("[\\s,]")) {
-          // Since we split on whitespaces also, there's no need to "trim" the resulting parts
-          // We only need to ignore the empty parts
-          if (!authority.isEmpty()) {
-            authorities.add(new SimpleGrantedAuthority(authority));
+    for (String attributeName : attributeNames) {
+      try {
+        String[] attributeValues = userData.getStringAttributes(attributeName);
+        // Should the attribute not be defined, the returned array is null
+        if (attributeValues != null) {
+          for (String attributeValue : attributeValues) {
+            // The attribute value may be a single authority (a single role) or a list of roles
+            for (String authority : attributeValue.split("[\\s,]")) {
+              // Since we split on whitespaces also, there's no need to "trim" the resulting parts
+              // We only need to ignore the empty parts
+              if (!authority.isEmpty()) {
+                authorities.add(new SimpleGrantedAuthority(authority));
+              }
+            }
           }
+        } else {
+          logger.debug("Could not find any attribute named '{}' in user '{}'", attributeName, userData.getDn());
         }
+      } catch (ClassCastException e) {
+        logger.error("Specified attribute containing user roles ('{}') was not of expected type String: {}",
+                attributeName, e);
       }
-      // TODO For some reason, Matterhorn requires the role anonymous...
-      // TODO Should we include it always, or only if no other roles are present?
-      authorities.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
-    } catch (ClassCastException e) {
-      logger.error("Specified attribute containing user roles ('{}') was not of expected type String: {}",
-              roleAttribute, e);
     }
+    // TODO For some reason, Matterhorn requires the role anonymous...
+    // TODO Should we include it always, or only if no other roles are present?
+    authorities.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
 
     return authorities;
   }
