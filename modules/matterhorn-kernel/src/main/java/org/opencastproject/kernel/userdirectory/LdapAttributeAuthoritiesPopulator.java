cb40177d5afb8b9ad8060736e3407256b3a6079f
Ruben Perez Vazquez <ruben.perez@uni-koeln.de>
2016-03-24 17:00:42 +0100

    MH-8577 : Improved Matterhorn authorities populator
    This version includes some properties to control its behaviour. In
    particular, properties have been added to:
    * Convert the roles to uppercase.
    * Add a prefix to every role.
    * Include some default roles that all users will have.
    
    The new version also handles whitespace differently: every whitespace
    sequence will be collapsed into a single "_" character. Sequences of "_"
    are similarly collapsed, too. This makes sure that all roles stick to
    certain minimum syntax rules.

--- modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/userdirectory/LdapAttributeAuthoritiesPopulator.java
+++ modules/matterhorn-kernel/src/main/java/org/opencastproject/kernel/userdirectory/LdapAttributeAuthoritiesPopulator.java
@@ -26,9 +26,13 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+/** Map a series of LDAP attributes to user authorities in Opencast */
 public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulator {
 
-  private String[] attributeNames;
+  private Set<String> attributeNames;
+  private String prefix = "";
+  private boolean uppercase = true;
+  private Set<String> additionalAuthorities;
   private static final Logger logger = LoggerFactory.getLogger(LdapAttributeAuthoritiesPopulator.class);
 
   /**
@@ -40,8 +44,18 @@ public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulat
    * @param attributeNames
    *          The LDAP user attribute containing the roles
    */
-  public LdapAttributeAuthoritiesPopulator(String... attributeNames) {
-    this.attributeNames = attributeNames;
+  public LdapAttributeAuthoritiesPopulator(Collection<? extends String> attributeNames) {
+    if (attributeNames == null) {
+      throw new NullPointerException("The attribute list cannot be null");
+    }
+
+    if (attributeNames.size() == 0) {
+      throw new IllegalArgumentException("At least one attribute must be provided");
+    }
+
+    this.attributeNames = new HashSet<String>(attributeNames);
+
+    this.additionalAuthorities = new HashSet<String>();
   }
 
   @Override
@@ -54,13 +68,7 @@ public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulat
         if (attributeValues != null) {
           for (String attributeValue : attributeValues) {
             // The attribute value may be a single authority (a single role) or a list of roles
-            for (String authority : attributeValue.split("[\\s,]")) {
-              // Since we split on whitespaces also, there's no need to "trim" the resulting parts
-              // We only need to ignore the empty parts
-              if (!authority.isEmpty()) {
-                authorities.add(new SimpleGrantedAuthority(authority));
-              }
-            }
+            addAuthorities(authorities, attributeValue.split(","));
           }
         } else {
           logger.debug("Could not find any attribute named '{}' in user '{}'", attributeName, userData.getDn());
@@ -70,11 +78,125 @@ public class LdapAttributeAuthoritiesPopulator implements LdapAuthoritiesPopulat
                 attributeName, e);
       }
     }
-    // TODO For some reason, Matterhorn requires the role anonymous...
-    // TODO Should we include it always, or only if no other roles are present?
-    authorities.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+
+    // Add the additional authorities
+    addAuthorities(authorities, additionalAuthorities.toArray(new String[additionalAuthorities.size()]));
 
     return authorities;
   }
 
+  /**
+   * Return the attributes names this object will search for
+   *
+   * @return a {@link Collection} containing such attribute names
+   */
+  public Collection<String> getAttributeNames() {
+    return new HashSet<String>(this.attributeNames);
+  }
+
+  /**
+   * Define the prefix that should be appended to all the roles obtained.
+   *
+   * The prefix is empty by default.
+   *
+   * @param prefix
+   *          A {@link String} containing the desired role prefix.
+   *
+   */
+  public void setRolePrefix(String prefix) {
+    this.prefix = (prefix == null) ? "" : prefix;
+  }
+
+  /**
+   * Get the role prefix being used by this object. Please note that such prefix can be empty.
+   *
+   * @return the role prefix in use.
+   */
+  public String getRolePrefix() {
+    return prefix;
+  }
+
+  /**
+   * Set whether or not to convert the role names to uppercase.
+   *
+   * Initially, this property defaults to {@code true}.
+   *
+   * @param convert
+   *          {@code true} to convert the role names to uppercase. {@code false} otherwise.
+   */
+  public void setConvertToUpperCase(boolean convert) {
+    uppercase = convert;
+  }
+
+  /**
+   * Get the property that defines whether or not the role names should be converted to uppercase.
+   *
+   * @return {@code true} if this class converts the role names to uppercase. {@code false} otherwise.
+   */
+  public boolean getConvertToUpperCase() {
+    return uppercase;
+  }
+
+  /**
+   * Define the set of additional authorities that are appended to the authority list of every authenticated user.
+   *
+   * The authorities must not be null nor empty.
+   *
+   * @param authorities
+   *          a {@link Collection} of {@link String}s representing the additional authorities.
+   */
+  public void setAdditionalAuthorities(Set<? extends String> authorities) {
+    if (authorities == null) {
+      additionalAuthorities.clear();
+    } else {
+      additionalAuthorities = new HashSet<String>(authorities);
+    }
+  }
+
+  /**
+   * Return the set of additional authorities, that are appended to the authority list of every authenticated user.
+   *
+   * @return a {@link Collection} of {@link String}s representing the additional authorities.
+   */
+  public Set<? extends String> getAdditionalAuthorities() {
+    return new HashSet<String>(additionalAuthorities);
+  }
+
+  private void addAuthorities(Set<GrantedAuthority> authorities, String[] values) {
+    for (String value : values) {
+      String authority = parseAuthority(value);
+      // Ignore the empty parts
+      if (!authority.isEmpty()) {
+        authorities.add(new SimpleGrantedAuthority(authority));
+      }
+    }
+  }
+
+  /**
+   * Processes a {@link String} representing a {@link GrantedAuthority} to make it comply with the expected format
+   *
+   * In particular, the method adds a prefix (if defined), capitalises the {@String}, substitutes the white spaces by
+   * underscores ("_") and collapses any series of underscores into a single one.
+   *
+   * @param authority
+   *          A {@code String} representing the authority
+   * @return a {@code String} representing the authority in a standard format
+   */
+  private String parseAuthority(String authority) {
+    // Trim the authority
+    // We do not substitute the whitespace here because we must make sure that the prefix follows the spacing
+    // conventions, too
+    authority = authority.trim();
+
+    // Add prefix only if the authority is not empty
+    if (!authority.isEmpty()) {
+      if (uppercase) {
+        return (this.prefix.trim() + authority).replaceAll("[\\s_]+", "_").toUpperCase();
+      } else {
+        return (this.prefix.trim() + authority).replaceAll("[\\s_]+", "_");
+      }
+    }
+
+    return authority;
+  }
 }
