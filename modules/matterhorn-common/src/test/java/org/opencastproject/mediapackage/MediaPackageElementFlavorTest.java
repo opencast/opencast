e2af7e812d015d04e8b075b33946488591ee9c27
Ruben Perez Vazquez <ruben.perez@uni-koeln.de>
2016-04-04 16:23:44 +0200

    MH-11391 : Improve flavor parsing and behaviour with "edge cases"
    Even though there is no document stating the exact flavor syntax, it is
    implicitly assumed that they should always have two non-blank parts
    separated with a slash ("/"). The current constructor did not ensure
    that the parts are non-empty and did not check that they did not content
    any "/" character (thus making it possible to create a flavor with e.g.
    `invalid/type` and `validsubtype` that would result in an illegal, but
    instantiable `invalid/type/validsubtype` flavor), and therefore required
    to be fixed.
    
    On the other hand, the `parseFlavor` method worked in a non-intuitive
    way by allowing arguments with more than two slash-separated components
    (e.g. "flavor/to/parse") to be successfully parsed, keeping the two
    first parts and discarding the rest. It was agreed that `parseFlavor`
    should fail for such cases, so this commit addresses that.
    
    Finally, some more unit tests demonstrating the new modifications have
    also been included.

--- modules/matterhorn-common/src/test/java/org/opencastproject/mediapackage/MediaPackageElementFlavorTest.java
+++ modules/matterhorn-common/src/test/java/org/opencastproject/mediapackage/MediaPackageElementFlavorTest.java
@@ -39,6 +39,14 @@ public class MediaPackageElementFlavorTest {
       new String[] { null, null },
       new String[] { "valid", null },
       new String[] { null, "valid" },
+      new String[] { "", "" },
+      new String[] { "valid", "" },
+      new String[] { "", "valid" },
+      new String[] { " \t \n \r ", " \t \n \r " },
+      new String[] { " \t \n \r ", "" },
+      new String[] { "", " \t \n \r " },
+      new String[] { "valid", " \t \n \r " },
+      new String[] { " \t \n \r ", "valid" },
       new String[] { "invalid/type", "valid_subtype" },
       new String[] { "valid_type", "invalid/subtype" }
     };
@@ -46,9 +54,9 @@ public class MediaPackageElementFlavorTest {
     for (String[] arguments : inputArguments) {
       try {
         new MediaPackageElementFlavor(arguments[0], arguments[1]);
-      } catch (Throwable e) {
-        assertEquals(format("Catched unexpected exception. The message was: %s", e.getMessage()),
-                IllegalArgumentException.class, e.getClass());
+      } catch (Throwable t) {
+        assertEquals(format("Catched unexpected exception. The message was: %s", t.getMessage()),
+                IllegalArgumentException.class, t.getClass());
         continue;
       }
       fail(format("Flavor constructor did not throw an expected exception with invalid arguments \"%s\" and \"%s\"",
@@ -57,25 +65,68 @@ public class MediaPackageElementFlavorTest {
   }
 
   @Test
+  public void testEquals() {
+    assertEquals("Leading or trailing whitespace in the flavor type should be trimmed",
+            flavor(" \r \n \t type \t \n \r ", "subtype"), flavor("type", "subtype"));
+    assertEquals("Leading or trailing whitespace in the flavor subtype should be ignored",
+            flavor("type", " \r \n \t subtype \t \n \r "), flavor("type", "subtype"));
+    assertEquals("Leading or trailing whitespace in both the flavor type and subtype should be trimmed",
+            flavor(" \r \n \t type \t \n \r ", " \r \n \t subtype \t \n \r "), flavor("type", "subtype"));
+    assertEquals("Flavor type should be case-insensitive", flavor("TYPE", "subtype"), flavor("type", "subtype"));
+    assertEquals("Flavor type should be case-insensitive", flavor("type", "SUBTYPE"), flavor("type", "subtype"));
+    assertEquals("Flavor parts should be case-insensitive", flavor("TYPE", "SUBTYPE"), flavor("type", "subtype"));
+  }
+
+  @Test
   public void testMatches() throws Exception {
-    assertFalse("null does not match", flavor("a", "b").matches(null));
-    assertTrue("Equal flavors match", flavor("a", "b").matches(flavor("a", "b")));
-    assertFalse("Unequal flavors do not match", flavor("a", "b").matches(flavor("b", "a")));
-    assertTrue("Wildcard type matches", flavor("*", "b").matches(flavor("a", "b")));
-    assertTrue("Match is commutative",
+    assertFalse("null should not match any flavor", flavor("a", "b").matches(null));
+    assertTrue("Equal flavors should match", flavor("a", "b").matches(flavor("a", "b")));
+    assertFalse("Unequal flavors should not match", flavor("a", "b").matches(flavor("b", "a")));
+    assertTrue("Wildcard type did not match", flavor("*", "b").matches(flavor("a", "b")));
+    assertTrue("Match with wildcard type should be commutative",
             flavor("a", "b").matches(flavor("*", "b")) && flavor("*", "b").matches(flavor("a", "b")));
-    assertTrue("Wildcard subtype matches", flavor("a", "*").matches(flavor("a", "b")));
-    assertTrue("Match is commutative",
+    assertTrue("Wildcard subtype did not match", flavor("a", "*").matches(flavor("a", "b")));
+    assertTrue("Match with wildcard subtype should be commutative",
             flavor("a", "b").matches(flavor("a", "*")) && flavor("a", "*").matches(flavor("a", "b")));
-    assertTrue("Wildcard matches", flavor("*", "*").matches(flavor("a", "b")));
-    assertTrue("Match is commutative",
+    assertTrue("Wildcard in both type and subtype did not match", flavor("*", "*").matches(flavor("a", "b")));
+    assertTrue("Match with wildcards in both type and subtype should be commutative",
             flavor("*", "*").matches(flavor("a", "b")) && flavor("a", "b").matches(flavor("*", "*")));
     assertTrue("Wildcard matches", flavor("*", "*").matches(flavor("*", "b")));
-    assertTrue("Match is commutative",
+    assertTrue("Match between wildcards should be commutative",
             flavor("*", "*").matches(flavor("*", "b")) && flavor("*", "b").matches(flavor("*", "*")));
     assertTrue("Wildcard matches", flavor("*", "*").matches(flavor("a", "*")));
-    assertTrue("Match is commutative",
+    assertTrue("Match between wildcards should be commutative",
             flavor("*", "*").matches(flavor("a", "*")) && flavor("a", "*").matches(flavor("*", "*")));
     assertTrue("Wildcard matches", flavor("*", "*").matches(flavor("*", "*")));
+  }
+
+  @Test
+  public void testParse() {
+
+    String[] invalidFlavors = new String[] {
+            null,
+            "",
+            "/",
+            "valid/",
+            "/valid",
+            " \r \n \t /",
+            " \r \n \t /valid",
+            "/ \r \n \t ",
+            "valid/ \r \n \t ",
+            " \r \n \t / \r \n \t ",
+            "too/many/slashes",
+            "/ \r \n \t too \r / \r many   / \t \r \n slashes \t"
+    };
+
+    for (String invalid : invalidFlavors) {
+      try {
+        MediaPackageElementFlavor.parseFlavor(invalid);
+      } catch (Throwable t) {
+        assertEquals(format("Catched unexpected exception. The message was: %s", t.getMessage()),
+                IllegalArgumentException.class, t.getClass());
+        continue;
+      }
+      fail(format("Invalid flavor should fail but was parsed without errors: \"%s\"", invalid));
+    }
   }
 }
