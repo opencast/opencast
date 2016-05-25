20ee13e18160c62f826b8a1ea662b821bf800d9c
Sven Stauber <sven.stauber@switch.ch>
2016-05-25 12:16:21 +0200

    Added Greek to the Theodul Player language mapping table

--- modules/matterhorn-engage-theodul-core/src/main/resources/ui/js/engage/core.js
+++ modules/matterhorn-engage-theodul-core/src/main/resources/ui/js/engage/core.js
@@ -181,6 +181,7 @@ define(["require", "jquery", "underscore", "backbone", "mousetrap", "bowser", "b
           case "sv": return "sv-SE";
           case "tr": return "tr-TR";
           case "zh": return "zh-CN";
+          case "el": return "el-GR";
           default: return null;
       }
   }
