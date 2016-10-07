4ee10e2b3456c2ec60a7ab398ec437de18165c61
Sven Stauber <sven.stauber@switch.ch>
2016-10-07 15:24:47 +0200

    Revert "Revert changes in module matterhorn-admin-ui-ng to develop"
    This reverts commit 6826ee2f722741dc7d0eaa1e3e888088760b9626.

--- modules/matterhorn-admin-ui-ng/src/test/resources/test/unit/modules/events/controllers/eventsControllerSpec.js
+++ modules/matterhorn-admin-ui-ng/src/test/resources/test/unit/modules/events/controllers/eventsControllerSpec.js
@@ -26,6 +26,7 @@ describe('Events controller', function () {
 
         it('deletes the event', function () {
             $httpBackend.expectGET('/admin-ng/resources/events/filters.json').respond('[]');
+            $httpBackend.expectGET('/admin-ng/resources/PUBLICATION.CHANNEL.LABELS.json').respond('{}');
             $httpBackend.expectDELETE('/admin-ng/event/12').respond('12');
 
             $scope.table.delete({'id': 12});
