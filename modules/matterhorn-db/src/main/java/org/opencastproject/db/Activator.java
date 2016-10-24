cd091de7b0f6f2184dbb59c61b3be75b71badab9
Lukas Rohner <lrohner@extron.com>
2016-10-24 16:10:24 +0200

    MH-10753 Added JDBC connection pool settings

--- modules/matterhorn-db/src/main/java/org/opencastproject/db/Activator.java
+++ modules/matterhorn-db/src/main/java/org/opencastproject/db/Activator.java
@@ -44,12 +44,11 @@ public class Activator implements BundleActivator {
   private static final Logger logger = LoggerFactory.getLogger(Activator.class);
 
   private String rootDir;
-  private ServiceRegistration datasourceRegistration;
+  private ServiceRegistration<?> datasourceRegistration;
   private ComboPooledDataSource pooledDataSource;
 
   @Override
   public void start(BundleContext bundleContext) throws Exception {
-
     // Use the configured storage directory
     rootDir = bundleContext.getProperty("org.opencastproject.storage.dir") + File.separator + "db";
 
@@ -60,17 +59,45 @@ public class Activator implements BundleActivator {
             "jdbc:h2:" + rootDir);
     String jdbcUser = getConfigProperty(bundleContext.getProperty("org.opencastproject.db.jdbc.user"), "sa");
     String jdbcPass = getConfigProperty(bundleContext.getProperty("org.opencastproject.db.jdbc.pass"), "sa");
+
+    Integer maxPoolSize = getConfigProperty(bundleContext.getProperty("org.opencastproject.db.jdbc.pool.max.size"));
+    Integer minPoolSize = getConfigProperty(bundleContext.getProperty("org.opencastproject.db.jdbc.pool.min.size"));
+    Integer acquireIncrement = getConfigProperty(
+            bundleContext.getProperty("org.opencastproject.db.jdbc.pool.acquire.increment"));
+    Integer maxStatements = getConfigProperty(
+            bundleContext.getProperty("org.opencastproject.db.jdbc.pool.max.statements"));
+    Integer loginTimeout = getConfigProperty(
+            bundleContext.getProperty("org.opencastproject.db.jdbc.pool.login.timeout"));
+    Integer maxIdleTime = getConfigProperty(
+            bundleContext.getProperty("org.opencastproject.db.jdbc.pool.max.idle.time"));
+    Integer maxConnectionAge = getConfigProperty(
+            bundleContext.getProperty("org.opencastproject.db.jdbc.pool.max.connection.age"));
+
     pooledDataSource = new ComboPooledDataSource();
     pooledDataSource.setDriverClass(jdbcDriver);
     pooledDataSource.setJdbcUrl(jdbcUrl);
     pooledDataSource.setUser(jdbcUser);
     pooledDataSource.setPassword(jdbcPass);
+    if (minPoolSize != null)
+      pooledDataSource.setMinPoolSize(minPoolSize);
+    if (maxPoolSize != null)
+      pooledDataSource.setMaxPoolSize(maxPoolSize);
+    if (acquireIncrement != null)
+      pooledDataSource.setAcquireIncrement(acquireIncrement);
+    if (maxStatements != null)
+      pooledDataSource.setMaxStatements(maxStatements);
+    if (loginTimeout != null)
+      pooledDataSource.setLoginTimeout(loginTimeout);
+    if (maxIdleTime != null)
+      pooledDataSource.setMaxIdleTime(maxIdleTime);
+    if (maxConnectionAge != null)
+      pooledDataSource.setMaxConnectionAge(maxConnectionAge);
 
     Connection connection = null;
     try {
       logger.info("Testing connectivity to database at {}", jdbcUrl);
       connection = pooledDataSource.getConnection();
-      Hashtable<String, String> dsProps = new Hashtable<String, String>();
+      Hashtable<String, String> dsProps = new Hashtable<>();
       dsProps.put("osgi.jndi.service.name", "jdbc/matterhorn");
       datasourceRegistration = bundleContext.registerService(DataSource.class.getName(), pooledDataSource, dsProps);
     } catch (SQLException e) {
@@ -90,11 +117,16 @@ public class Activator implements BundleActivator {
     logger.info("Shutting down database");
     if (datasourceRegistration != null)
       datasourceRegistration.unregister();
+    logger.info("Shutting down connection pool");
     DataSources.destroy(pooledDataSource);
   }
 
   private String getConfigProperty(String config, String defaultValue) {
     return config == null ? defaultValue : config;
+  }
+
+  private Integer getConfigProperty(String config) {
+    return config == null ? null : Integer.parseInt(config);
   }
 
 }
