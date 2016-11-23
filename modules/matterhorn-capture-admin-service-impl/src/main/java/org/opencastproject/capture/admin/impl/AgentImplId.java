f552ba88b62cca0d77b600120ef6e42999b5b444
Lars Kiesow <lkiesow@uos.de>
2016-11-23 23:34:36 +0100

    MH-11904, Fixed Missing IdClass
    For several classes the following warnings appears in the logs:
    
      You have specified multiple ids for the entity class without
      specifying an @IdClass
    
    This fixes that problem.
    
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

---
+++ modules/matterhorn-capture-admin-service-impl/src/main/java/org/opencastproject/capture/admin/impl/AgentImplId.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.opencastproject.capture.admin.impl;
+
+class AgentImplId {
+  private String name;
+  private String organization;
+
+  AgentImplId(String name, String organization) {
+    this.name = name;
+    this.organization = organization;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getOrganization() {
+    return organization;
+  }
+
+  public void setOrganization(String organization) {
+    this.organization = organization;
+  }
+}
