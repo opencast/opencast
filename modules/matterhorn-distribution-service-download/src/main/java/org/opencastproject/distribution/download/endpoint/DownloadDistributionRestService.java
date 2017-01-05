ea15934775fbbf5d80b3589e2d0c4d67fb034507
cgreweling <cgreweling@greweling.virtuos.uni-osnabrueck.de>
2017-01-05 16:36:26 +0100

    Changed id(string) to id(json-string) for  retract operation

--- modules/matterhorn-distribution-service-download/src/main/java/org/opencastproject/distribution/download/endpoint/DownloadDistributionRestService.java
+++ modules/matterhorn-distribution-service-download/src/main/java/org/opencastproject/distribution/download/endpoint/DownloadDistributionRestService.java
@@ -126,7 +126,7 @@ public class DownloadDistributionRestService extends AbstractJobProducerEndpoint
                                     type = Type.TEXT),
                      @RestParameter(name = "elementId",
                                     isRequired = true,
-                                    description = "The element to distribute. ID or multiple Ids in JSON Format (['IdOne','IdTwo'])",
+                                    description = "The element to distribute. The Id or multiple Ids as JSON Array ( ['IdOne','IdTwo'] )",
                                     type = Type.STRING) },
              reponses = {
                      @RestResponse(responseCode = SC_OK,
@@ -138,9 +138,9 @@ public class DownloadDistributionRestService extends AbstractJobProducerEndpoint
           throws Exception {
     try {
       Gson gson = new Gson();
-      Set<String> elementIds = gson.fromJson(elementId,new TypeToken<Set<String>>() { }.getType());
+      Set<String> setElementIds = gson.fromJson(elementId, new TypeToken<Set<String>>() { }.getType());
       final MediaPackage mediapackage = MediaPackageParser.getFromXml(mediaPackageXml);
-      final Job job = service.distribute(channelId, mediapackage, elementIds, checkAvailability);
+      final Job job = service.distribute(channelId, mediapackage, setElementIds, checkAvailability);
       return ok(new JaxbJob(job));
     } catch (IllegalArgumentException e) {
       logger.debug("Unable to distribute element: {}", e.getMessage());
@@ -166,7 +166,7 @@ public class DownloadDistributionRestService extends AbstractJobProducerEndpoint
                                     type = Type.TEXT),
                      @RestParameter(name = "elementId",
                                     isRequired = true,
-                                    description = "The element to retract",
+                                    description = "The element to retract. The Id or multiple Ids as JSON Array ( ['IdOne','IdTwo'] )",
                                     type = Type.STRING) },
              reponses = {
                      @RestResponse(responseCode = SC_OK,
@@ -176,8 +176,10 @@ public class DownloadDistributionRestService extends AbstractJobProducerEndpoint
                           @FormParam("channelId") String channelId)
           throws Exception {
     try {
+      Gson gson = new Gson();
+      Set<String> setElementIds = gson.fromJson(elementId, new TypeToken<Set<String>>() { }.getType());
       final MediaPackage mediapackage = MediaPackageParser.getFromXml(mediaPackageXml);
-      final Job job = service.retract(channelId, mediapackage, elementId);
+      final Job job = service.retract(channelId, mediapackage, setElementIds);
       return ok(new JaxbJob(job));
     } catch (IllegalArgumentException e) {
       logger.debug("Unable to retract element: {}", e.getMessage());
