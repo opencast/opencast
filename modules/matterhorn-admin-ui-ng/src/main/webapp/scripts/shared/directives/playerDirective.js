49ccde6507b0dfb32f80c71efaed2a6c5003b48a
Sven Stauber <sven.stauber@switch.ch>
2017-11-29 13:27:19 +0100

    Be more defensive when accessing the player adapter to avoid errors in case the player adapter could not be instantiated

--- modules/matterhorn-admin-ui-ng/src/main/webapp/scripts/shared/directives/playerDirective.js
+++ modules/matterhorn-admin-ui-ng/src/main/webapp/scripts/shared/directives/playerDirective.js
@@ -103,7 +103,9 @@ angular.module('adminNg.directives')
 
             scope.$on('$destroy', function () {
                 $timeout.cancel(scope.checkTimeout);
-                scope.player.adapter.pause();
+                if (angular.isDefined(scope.player.adapter)) {
+                  scope.player.adapter.pause();
+                }
             });
 
             scope.previousFrame = function () {
