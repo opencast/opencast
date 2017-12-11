248713dc8ce7991f9e109ea8b6af614bc652d6c0
Greg Logan <gregorydlogan@gmail.com>
2017-12-11 11:39:36 -0600

    MH-12464: Addressing issue identified at Manchester where MSSQL complains that mh_host_registration.host is not in the GROUP_BY clause

--- modules/matterhorn-common-jpa-impl/src/main/java/org/opencastproject/serviceregistry/impl/jpa/ServiceRegistrationJpaImpl.java
+++ modules/matterhorn-common-jpa-impl/src/main/java/org/opencastproject/serviceregistry/impl/jpa/ServiceRegistrationJpaImpl.java
@@ -66,7 +66,7 @@ import javax.persistence.UniqueConstraint;
                 + "WHERE job.processorServiceRegistration.online=true and job.processorServiceRegistration.active=true and job.processorServiceRegistration.hostRegistration.maintenanceMode=false "
                 + "AND job.status in :statuses "
                 + "AND job.processorServiceRegistration.serviceType != :workflow_type "
-                + "GROUP BY job.processorServiceRegistration, job.status"),
+                + "GROUP BY job.processorServiceRegistration.hostRegistration.baseUrl, job.status"),
         @NamedQuery(name = "ServiceRegistration.getRegistration", query = "SELECT r from ServiceRegistration r "
                 + "where r.hostRegistration.baseUrl = :host and r.serviceType = :serviceType"),
         @NamedQuery(name = "ServiceRegistration.getAll", query = "SELECT rh FROM ServiceRegistration rh WHERE rh.hostRegistration.active = true"),
