7b52e7f08b280f2ee4f8cccb1bdc326127b89ee6
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-03-08 22:36:54 +0100

    Don't Duplicate Inputs in editableSingleSelect.html

--- modules/admin-ui/src/main/webapp/scripts/shared/directives/editableSingleSelectDirective.js
+++ modules/admin-ui/src/main/webapp/scripts/shared/directives/editableSingleSelectDirective.js
@@ -22,7 +22,7 @@
    </doc:example>
  */
 angular.module('adminNg.directives')
-.directive('adminNgEditableSingleSelect', ['$timeout', function ($timeout) {
+.directive('adminNgEditableSingleSelect', ['$timeout', '$filter', function ($timeout, $filter) {
     return {
         restrict: 'A',
         templateUrl: 'shared/partials/editableSingleSelect.html',
@@ -35,21 +35,21 @@ angular.module('adminNg.directives')
         },
         link: function (scope, element) {
 
-            var mapToArray = function (map) {
+            var mapToArray = function (map, translate) {
                 var array = [];
 
                 angular.forEach(map, function (mapValue, mapKey) {
 
                     array.push({
-                        label: mapKey,
+                        label: translate ? $filter('translate')(mapKey) : mapKey,
                         value: mapValue
                     });
                 });
 
-                return array;
+                return $filter('orderBy')(array, 'label');
             }
 
-            var mapToArrayOrdered = function (map) {
+            var mapToArrayOrdered = function (map, translate) {
                 var array = [];
 
                 angular.forEach(map, function (mapValue, mapKey) {
@@ -66,7 +66,7 @@ angular.module('adminNg.directives')
                 });
                 return array.map(function (entry) {
                     return {
-                        label: entry.label.label,
+                        label: translate ? $filter('translate')(entry.label.label) : entry.label.label,
                         value: entry.value
                     };
                 });
@@ -74,7 +74,8 @@ angular.module('adminNg.directives')
 
 
             //transform map to array so that orderBy can be used
-            scope.collection = scope.ordered ? mapToArrayOrdered(scope.collection) : mapToArray(scope.collection);
+            scope.collection = scope.ordered ? mapToArrayOrdered(scope.collection, scope.params.translatable) :
+                mapToArray(scope.collection, scope.params.translatable);
 
             scope.submit = function () {
                 // Wait until the change of the value propagated to the parent's
