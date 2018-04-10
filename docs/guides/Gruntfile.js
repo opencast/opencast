63b544e1a9f15e4a3ac231e9d00ab517700edd78
Daniel Ziegenberg <daniel@ziegenberg.at>
2018-04-10 18:14:31 +0200

    add rule MD004, MD005, MD006 and MD007 for markdownlint

--- docs/guides/Gruntfile.js
+++ docs/guides/Gruntfile.js
@@ -1,30 +1,34 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   //to use via grunt, first load the task
-  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
 
   grunt.initConfig({
     markdownlint: {
       full: {
         options: {
-          config: { // configure the linting rules
-            'default': false, // in the beginning disable all rules and enable them on a one-by-one basis
-            'line-length': { // MD013
-              "line_length": 120,
-              code_blocks: false,
-              tables: false,
+          config: {
+            // configure the linting rules
+            default: false, // in the beginning disable all rules and enable them on a one-by-one basis
+            MD004: { // unordered list style
+              style: "consistent"
             },
-            'accessibility': true,
+            MD005: true, // inconsistent indentation for list items at the same level
+            MD006: true, // consider starting bulleted lists at the beginning of the line
+            MD007: { // unordered list indentation
+              indent: 2
+            },
+            MD013: { // line-length
+              line_length: 120,
+              code_blocks: false,
+              tables: false
+            },
+            MD045: true, // accessibility
           }
         },
-        src: [
-          './admin/**/*.md',
-          './developer/**/*.md',
-          './user/**/*.md',
-        ]
+        src: ["./admin/**/*.md", "./developer/**/*.md", "./user/**/*.md"]
       }
     }
   });
 
-  grunt.registerTask('default', ['markdownlint']);
-
+  grunt.registerTask("default", ["markdownlint"]);
 };
