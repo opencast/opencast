ca3cc2e41cd22139d29ed5882e3ddd433d756e2b
Lars Kiesow <lkiesow@uos.de>
2018-04-11 00:50:33 +0200

    Enable Crowdin auto-upload
    This patch will automatically upload the translation sources to crowdin
    if the build and the tests do not fail.
    
    Additional notes:
    
    - The upload will only be executed on release branches and on develop.
      It will be skipped on all other branches.
    - The upload requires a secret `CROWDIN_API_KEY` variable to be set.
    - A failing upload will not break a build.

---
+++ .upload-crowdin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+# Check for pull requests
+if [ "${TRAVIS_PULL_REQUEST}" != false ]; then
+  echo "No crowdin upload on pull requests"
+  return 0
+fi
+# Check for non release branches or develop
+if ! echo "${TRAVIS_BRANCH}" | grep -Eq '^(r/[0-9]+\.x|develop)$'; then
+  echo "crowdin upload is only allowrd on release branches and develop, not on $TRAVIS_BRANCH"
+  return 0
+fi
+# Crowdin branches do not use the `r/` prefix
+CROWDIN_BRANCH="${TRAVIS_BRANCH//r\//}"
+wget --quiet https://artifacts.crowdin.com/repo/deb/crowdin.deb
+sudo dpkg -i crowdin.deb
+echo "api_key: ${CROWDIN_API_KEY}" > ~/.crowdin.yaml
+crowdin --config .crowdin.yaml upload sources -b "${CROWDIN_BRANCH}"
