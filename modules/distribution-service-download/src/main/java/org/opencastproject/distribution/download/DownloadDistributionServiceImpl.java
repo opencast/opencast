d4b1d25bf9dff90397a7bf5b153a4d7961d79105
Lars Kiesow <lkiesow@uos.de>
2018-05-18 00:43:28 +0200

    MH-12898, Fix download distribution job load defaults
    This patch fixes the default values for the download distribution job
    loads. It increases the default value for distribution jobs since they
    can be slightly more expensive due to file transfers though it does not
    go so far as to raise it all the way up to one like the configuration
    file did.
    
    The retract job load is lowered to 0.1 since it is a very simple job
    after all and a system should be able to handle multiple retract jobs in
    parallel.

--- modules/distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
+++ modules/distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
@@ -100,10 +100,10 @@ public class DownloadDistributionServiceImpl extends AbstractDistributionService
   private static final long TIMEOUT = 60000L;
 
   /** The load on the system introduced by creating a distribute job */
-  public static final float DEFAULT_DISTRIBUTE_JOB_LOAD = 0.1f;
+  public static final float DEFAULT_DISTRIBUTE_JOB_LOAD = 0.2f;
 
   /** The load on the system introduced by creating a retract job */
-  public static final float DEFAULT_RETRACT_JOB_LOAD = 1.0f;
+  public static final float DEFAULT_RETRACT_JOB_LOAD = 0.1f;
 
   /** The key to look for in the service configuration file to override the {@link DEFAULT_DISTRIBUTE_JOB_LOAD} */
   public static final String DISTRIBUTE_JOB_LOAD_KEY = "job.load.download.distribute";
