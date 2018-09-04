29c1131ca978b610da2962db64ae0c18b3bf236a
Miguel Escriva <miesgre@gmail.com>
2018-09-04 14:42:30 +0200

    Update to paella 6.0.x

---
+++ modules/engage-paella-player/src/main/paella-opencast/plugins/es.upv.paella.opencast.loader/03_oc_search_converter.js
@@ -0,0 +1,313 @@
+class OpencastToPaellaConverter {
+
+  constructor() {        
+  }
+
+  getVideoTypeFromTrack(track) {
+    var videoType = null;
+
+    var protocol = /^(.*):\/\/.*$/.exec(track.url);
+    if (protocol) {
+      switch(protocol[1]) {
+      case 'rtmp':
+      case 'rtmps':
+        switch (track.mimetype) {
+        case 'video/mp4':
+        case 'video/ogg':
+        case 'video/webm':
+        case 'video/x-flv':
+          videoType = 'rtmp';
+          break;
+        default:
+          paella.debug.log(`OpencastToPaellaConverter: MimeType (${track.mimetype}) not supported!`);
+          break;
+        }
+        break;
+      case 'http':
+      case 'https':
+        switch (track.mimetype) {
+        case 'video/mp4':
+        case 'video/ogg':
+        case 'video/webm':
+          videoType = track.mimetype.split('/')[1];
+          break;
+        case 'video/x-flv':
+          videoType = 'flv';
+          break;
+        case 'application/x-mpegURL':
+          videoType = 'hls';
+          break;
+        case 'application/dash+xml':
+          videoType = 'mpd';
+          break;
+        
+        default:
+          paella.debug.log(`OpencastToPaellaConverter: MimeType (${track.mimetype}) not supported!`);
+          break;
+        }
+        break;
+      default:
+        paella.debug.log(`OpencastToPaellaConverter: Protocol (${protocol[1]}) not supported!`);
+        break;
+      }
+    }
+
+    return videoType;
+  }
+
+  getStreamSourceFromTrack(track) {
+    var res = new Array(0,0);
+    if (track.video instanceof Object) {
+      res = track.video.resolution.split('x');
+    }
+    
+    var src = track.url;
+    var urlSplit = /^(rtmps?:\/\/[^/]*\/[^/]*)\/(.*)$/.exec(track.url);
+    if (urlSplit != null) {
+      var rtmp_server =  urlSplit[1];
+      var rtmp_stream =  urlSplit[2];
+      src = {
+        server: encodeURIComponent(rtmp_server),
+        stream: encodeURIComponent(rtmp_stream)
+      };
+    }
+    
+    var source = {
+      src:  src,
+      mimetype: track.mimetype,
+      res: {w:res[0], h:res[1]},
+      isLiveStream: (track.live===true)
+    };
+    
+    return source;
+  }
+
+  getStreamFromFlavour(episode, mainFlavour) {
+    var currentStream = { sources:{}, preview: '', content: mainFlavour };        
+    
+    var tracks = episode.mediapackage.media.track;
+    var attachments = episode.mediapackage.attachments.attachment;
+    if (!(tracks instanceof Array)) { tracks = [tracks]; }
+    if (!(attachments instanceof Array)) { attachments = [attachments]; }
+
+    // Read the tracks!!
+    tracks.forEach((currentTrack) => {
+      if (currentTrack.type == `${mainFlavour}/delivery`) {
+        var videoType = this.getVideoTypeFromTrack(currentTrack);
+        if (videoType){
+          if ( !(currentStream.sources[videoType]) || !(currentStream.sources[videoType] instanceof Array)){
+            currentStream.sources[videoType] = [];
+          }
+          currentStream.sources[videoType].push(this.getStreamSourceFromTrack(currentTrack));
+        }
+      }                        
+    });         
+
+    // Read the attachments
+    var duration = parseInt(episode.mediapackage.duration/1000);
+    var imageSource =   {type:'image/jpeg', frames:{}, count:0, duration: duration, res:{w:320, h:180}};
+    var imageSourceHD = {type:'image/jpeg', frames:{}, count:0, duration: duration, res:{w:1280, h:720}};    
+    attachments.forEach((currentAttachment) => {            
+      if (currentAttachment.type == `${mainFlavour}/player+preview`) {
+        currentStream.preview = currentAttachment.url;
+      }
+      else if (currentAttachment.type == `${mainFlavour}/segment+preview+hires`) {
+        if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+          time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+          imageSourceHD.frames['frame_'+time] = currentAttachment.url;
+          imageSourceHD.count = imageSourceHD.count + 1;
+        }
+      }
+      else if (currentAttachment.type == `${mainFlavour}/segment+preview`) {
+        if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+          var time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+          imageSource.frames['frame_'+time] = currentAttachment.url;
+          imageSource.count = imageSource.count + 1;      
+        }
+      }            
+    });
+
+    var imagesArray = [];
+    if (imageSource.count > 0) {
+      imagesArray.push(imageSource);
+    }
+    if (imageSourceHD.count > 0) {
+      imagesArray.push(imageSourceHD);
+    }
+    if (imagesArray.length > 0) {
+      currentStream.sources.image = imagesArray;
+    }
+
+    return currentStream;               
+  }
+
+  getContentToImport(episode) {
+    var flavours = [];
+    var tracks = episode.mediapackage.media.track;    
+    if (!(tracks instanceof Array)) { tracks = [tracks]; }
+
+    tracks.forEach((currentTrack) => {
+      if (flavours.indexOf(currentTrack.type) < 0) {
+        flavours.push(currentTrack.type);
+      }
+    });
+
+    return flavours;
+  }    
+
+  getStreams(episode) {
+    // Get the streams
+    var paellaStreams = [];
+    var flavours = this.getContentToImport(episode);
+    flavours.forEach((flavour) => {
+      var flavourSplit = flavour.split('/');
+      if (flavourSplit[1] == 'delivery') {
+        var s = this.getStreamFromFlavour(episode, flavourSplit[0]);
+        paellaStreams.push(s);
+      }
+    });
+    return paellaStreams;
+  }
+        
+  getCaptions(episode) {
+    var captions = [];
+
+    var attachments = episode.mediapackage.attachments.attachment;
+    var catalogs = episode.mediapackage.metadata.catalog;    
+    if (!(attachments instanceof Array)) { attachments = [attachments]; }
+    if (!(catalogs instanceof Array)) { catalogs = [catalogs]; }
+
+
+    // Read the attachments
+    attachments.forEach((currentAttachment) => {
+      try {
+        let captions_regex = /^captions\/([^+]+)(\+(.+))?/g;
+        let captions_match = captions_regex.exec(currentAttachment.type);
+      
+        if (captions_match) {
+          let captions_format = captions_match[1];
+          let captions_lang = captions_match[3];
+
+          // TODO: read the lang from the dfxp file
+          //if (captions_format == "dfxp") {}
+
+          if (!captions_lang && currentAttachment.tags && currentAttachment.tags.tag) {
+            if (!(currentAttachment.tags.tag instanceof Array)) {
+              currentAttachment.tags.tag = [currentAttachment.tags.tag];
+            }
+            currentAttachment.tags.tag.forEach((tag)=>{
+              if (tag.startsWith('lang:')){
+                let split = tag.split(':');
+                captions_lang = split[1];
+              }
+            });
+          }
+
+          let captions_label = captions_lang || 'unknown language'; //base.dictionary.translate("CAPTIONS_" + captions_lang);
+
+          captions.push({
+            id: currentAttachment.id,
+            lang: captions_lang,
+            text: captions_label,
+            url: currentAttachment.url,
+            format: captions_format
+          });
+        }
+      }
+      catch (err) {/**/}
+    });
+
+    // Read the catalogs
+    catalogs.forEach((currentCatalog) => {
+      try {
+        // backwards compatibility:
+        // Catalogs flavored as 'captions/timedtext' are assumed to be dfxp
+        if (currentCatalog.type == 'captions/timedtext') {
+          let captions_lang;
+    
+          if (currentCatalog.tags && currentCatalog.tags.tag) {
+            if (!(currentCatalog.tags.tag instanceof Array)) {
+              currentCatalog.tags.tag = [currentCatalog.tags.tag];
+            }
+            currentCatalog.tags.tag.forEach((tag)=>{
+              if (tag.startsWith('lang:')){
+                let split = tag.split(':');
+                captions_lang = split[1];
+              }
+            });
+          }
+        
+          let captions_label = captions_lang || 'unknown language';
+          captions.push({
+            id: currentCatalog.id,
+            lang: captions_lang,
+            text: captions_label,
+            url: currentCatalog.url,
+            format: 'dfxp'
+          });
+        }
+      }
+      catch (err) {/**/}
+    });
+
+    return captions;
+  }
+
+  getSegments(episode) {
+    var segments = [];
+
+    var attachments = episode.mediapackage.attachments.attachment;
+    if (!(attachments instanceof Array)) { attachments = [attachments]; }
+
+    // Read the attachments
+    var opencastFrameList = {};
+    attachments.forEach((currentAttachment) => {
+      try {
+        if (currentAttachment.type == 'presentation/segment+preview+hires') {
+          if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+            time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+          
+            if (!(opencastFrameList[time])){
+              opencastFrameList[time] = {id:'frame_'+time, mimetype:currentAttachment.mimetype, time:time, url:currentAttachment.url, thumb:currentAttachment.url};
+            }
+            opencastFrameList[time].url = currentAttachment.url;
+          }
+        }
+        else if (currentAttachment.type == 'presentation/segment+preview') {
+          if (/time=T(\d+):(\d+):(\d+)/.test(currentAttachment.ref)) {
+            var time = parseInt(RegExp.$1)*60*60 + parseInt(RegExp.$2)*60 + parseInt(RegExp.$3);
+            if (!(opencastFrameList[time])){
+              opencastFrameList[time] = {id:'frame_'+time, mimetype:currentAttachment.mimetype, time:time, url:currentAttachment.url, thumb:currentAttachment.url};
+            }
+            opencastFrameList[time].thumb = currentAttachment.url;
+          }
+        }
+      }
+      catch (err) {/**/}
+    });
+
+    Object.keys(opencastFrameList).forEach((key, index) => {
+      segments.push(opencastFrameList[key]);
+    });        
+    return segments;
+  }
+
+
+  convertToDataJson(episode) {
+    var streams = this.getStreams(episode);
+    var captions = this.getCaptions(episode);
+    var segments = this.getSegments(episode);
+
+    var data =  {
+      metadata: {
+        title: episode.mediapackage.title,
+        duration: episode.mediapackage.duration/1000
+      },
+      streams: streams,
+      frameList: segments,
+      captions: captions
+    };
+
+    return data;
+  }
+}
