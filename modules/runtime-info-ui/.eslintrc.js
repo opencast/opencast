345ac0d5f97892ff2a4bd0c5d61c4666f7b5f4d9
Lars Kiesow <lkiesow@uos.de>
2018-09-12 22:51:35 +0200

    MH-13095, Add linter (checkstyle) for JavaScript
    For a long time, Opencast has an automated style checker for all Java
    code included in the project which ensured a mostly homogeneous style.
    For JavaScript, we talked about introducing checks for a while but never
    made any progress.
    
    This patch introduces eslint as example linter for the runtime-info-ui
    module. From here, this can be extended to other modules. Rules are not
    configured to be very strict. The most notably enforcement is probably
    the 2 space indention rule.

---
+++ modules/runtime-info-ui/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 2015
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
