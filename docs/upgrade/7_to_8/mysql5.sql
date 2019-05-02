f8bf15db52691aeab517989f2a1575b6c238365b
James S Perrin <james.s.perrin@manchester.ac.uk>
2019-05-02 10:49:04 +0100

    MH-13514, use correct table name prefix

--- docs/upgrade/7_to_8/mysql5.sql
+++ docs/upgrade/7_to_8/mysql5.sql
@@ -1,2 +1,2 @@
 -- Due to MH-13514 Add descriptive node names to hosts
-ALTER TABLE mh_host_registration ADD COLUMN node_name VARCHAR(255) AFTER host;
+ALTER TABLE oc_host_registration ADD COLUMN node_name VARCHAR(255) AFTER host;
