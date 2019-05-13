1fadc74c27c7e9760039c4ba4a37cd2fafa0c960
Sven Stauber <sven.stauber@switch.ch>
2019-05-13 12:34:14 +0200

    Fix broken build

--- modules/textanalyzer-workflowoperation/src/main/java/org/opencastproject/workflow/handler/textanalyzer/TextAnalysisWorkflowOperationHandler.java
+++ modules/textanalyzer-workflowoperation/src/main/java/org/opencastproject/workflow/handler/textanalyzer/TextAnalysisWorkflowOperationHandler.java
@@ -84,8 +84,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 
 /**
