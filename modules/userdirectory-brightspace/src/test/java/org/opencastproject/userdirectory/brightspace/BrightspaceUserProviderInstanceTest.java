b83098e71a04bc5f7ab98616fb3b342a91bd7abe
Kristof Keppens <kkeppens@gmail.com>
2019-05-16 14:05:12 +0200

    add dependencies to main pom and fix checkstyle erros

--- modules/userdirectory-brightspace/src/test/java/org/opencastproject/userdirectory/brightspace/BrightspaceUserProviderInstanceTest.java
+++ modules/userdirectory-brightspace/src/test/java/org/opencastproject/userdirectory/brightspace/BrightspaceUserProviderInstanceTest.java
@@ -2,27 +2,26 @@
  * Licensed to The Apereo Foundation under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
- * <p>
- * <p>
+ *
+ *
  * The Apereo Foundation licenses this file to you under the Educational
  * Community License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License
  * at:
- * <p>
- * http://opensource.org/licenses/ecl2.txt
- * <p>
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations under
  * the License.
+ *
  */
 
 package org.opencastproject.userdirectory.brightspace;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -30,15 +29,12 @@ import static org.mockito.Mockito.when;
 
 import org.opencastproject.security.api.Organization;
 import org.opencastproject.security.api.User;
-import org.opencastproject.userdirectory.brightspace.client.api.Activation;
 import org.opencastproject.userdirectory.brightspace.client.BrightspaceClient;
 import org.opencastproject.userdirectory.brightspace.client.BrightspaceClientException;
+import org.opencastproject.userdirectory.brightspace.client.api.Activation;
 import org.opencastproject.userdirectory.brightspace.client.api.BrightspaceUser;
-import org.opencastproject.userdirectory.brightspace.client.api.OrgUnit;
-import org.opencastproject.userdirectory.brightspace.client.api.OrgUnitItem;
-import org.opencastproject.userdirectory.brightspace.client.api.Role;
-import org.opencastproject.userdirectory.brightspace.client.api.Type;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -79,11 +75,11 @@ public class BrightspaceUserProviderInstanceTest {
     verify(client).getURL();
     verifyNoMoreInteractions(client);
 
-    assertThat(user.getUsername(), is("username"));
+    Assert.assertThat(user.getUsername(), is("username"));
   }
 
   public void shouldReturnProviderName() {
-    assertThat(brightspaceUserProviderInstance.getName(), is("brightspace"));
+    Assert.assertThat(brightspaceUserProviderInstance.getName(), is("brightspace"));
   }
 
 
