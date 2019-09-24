4c8fdfb59ea1b7d3c0ee0bfd76758b646d9afd6d
Eugen Kaiser <ek926m@gmail.com>
2019-09-24 14:49:05 +0200

    close esc function for new event and new series modals

--- modules/admin-ui/src/main/webapp/scripts/modules/events/controllers/eventController.js
+++ modules/admin-ui/src/main/webapp/scripts/modules/events/controllers/eventController.js
@@ -984,4 +984,18 @@ angular.module('adminNg.controllers')
       cleanupScopeResources();
     });
   }
-]);
+
+])
+.directive('escKey', function() {
+  return function(scope, element, attrs) {
+    element.bind('keydown keypress', function(event) {
+      var keyCode = event.which || event.keyCode;
+      if (keyCode === 27) {
+        scope.$apply(function() {
+          scope.$eval(attrs.escKey);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+});
--- modules/admin-ui/src/main/webapp/scripts/modules/events/controllers/newSeriesController.js
+++ modules/admin-ui/src/main/webapp/scripts/modules/events/controllers/newSeriesController.js
@@ -135,4 +135,17 @@ angular.module('adminNg.controllers')
     });
 
     $scope.components = ResourcesListResource.get({ resource: 'components' });
-  }]);
+  }])
+  .directive('escKey', function() {
+    return function(scope, element, attrs) {
+      element.bind('keydown keypress', function(event) {
+        var keyCode = event.which || event.keyCode;
+        if (keyCode === 27) {
+          scope.$apply(function() {
+            scope.$eval(attrs.escKey);
+          });
+          event.preventDefault();
+        }
+      });
+    };
+  });
