cc620abe8083c199863e1bcb4de02d7000a183d4
Lars Kiesow <lkiesow@uos.de>
2019-09-27 21:57:26 +0200

    Fix displaying tracks with no tags in player
    This patch fixes an error in the Theodul player which will result in an
    error if one of the published tracks has no tags.

--- modules/engage-theodul-plugin-video-videojs/src/main/resources/static/main.js
+++ modules/engage-theodul-plugin-video-videojs/src/main/resources/static/main.js
@@ -316,8 +316,9 @@ define(['require', 'jquery', 'underscore', 'backbone', 'basil', 'bowser', 'engag
     var newTracksArray = [];
 
     for (var i = 0; i < tracks.length; i++) {
-      var found = false;
-      for (var j = 0; j < tracks[i].tags.tag.length; j++) {
+      var found = false,
+          number_of_tags = tracks[i].tags ? tracks[i].tags.tag.length : 0;
+      for (var j = 0; j < number_of_tags; j++) {
         for (var k = 0; k < filterTags.length; k++) {
           if (tracks[i].tags.tag[j] == filterTags[k].trim()) {
             found = true;
@@ -350,7 +351,8 @@ define(['require', 'jquery', 'underscore', 'backbone', 'basil', 'bowser', 'engag
 
     for (var v in videoSources) {
       for (var i = 0; i < videoSources[v].length; i++) {
-        for (var j = 0; j < videoSources[v][i].tags.tag.length; j++) {
+        var number_of_tags = videoSources[v][i].tags ? videoSources[v][i].tags.tag.length : 0;
+        for (var j = 0; j < number_of_tags; j++) {
           if (keyword !== undefined) {
             if (videoSources[v][i].tags.tag[j].indexOf(keyword) > 0) {
               tagList.push(videoSources[v][i].tags.tag[j]);
