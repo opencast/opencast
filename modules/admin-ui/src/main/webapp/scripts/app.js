cd708c717f3077a2694b3148eb1ad88391429858
Eugen Kaiser <ek926m@gmail.com>
2019-10-01 19:37:32 +0200

    move to global app directive

--- modules/admin-ui/src/main/webapp/scripts/app.js
+++ modules/admin-ui/src/main/webapp/scripts/app.js
@@ -129,4 +129,17 @@ angular.module('adminNg', [
   $rootScope.toURL = function ( path ) {
     location.href = path;
   };
-}]);
+}])
+.directive('escKey', function() {
+  return function(scope, element, attrs) {
+    element.bind('keydown keypress', function(event) {
+      var keyCode = event.which || event.keyCode;
+      if (keyCode === 27) {
+        scope.$apply(function() {
+          scope.$eval(attrs.escKey);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+});
