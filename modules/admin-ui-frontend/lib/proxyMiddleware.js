bdc62411967aa514627e6116b7dc9d3e3eafab1a
Isabella Kutger <isabella.kutger@tik.uni-stuttgart.de>
2020-02-18 16:53:00 +0100

    Split AdminUI in Java and JavaScript parts (#1371)
    This patch splits the admin ui into two parts:
    
    - the Java backend (remains at admin-ui)
    - the JavaScript frontend (moved to admin-ui-frontend)
    
    Purpose of this split is to facilitate migrating from AngularJS (1.x) to Angular (8+) in a new module in parallel to the current admin-ui.

---
+++ modules/admin-ui-frontend/lib/proxyMiddleware.js
@@ -0,0 +1,129 @@
+'use strict';
+var proxyMiddleware = require('grunt-middleware-proxy/lib/Utils').getProxyMiddleware();
+var requestDigest = require('request-digest');
+var serveStatic = require('serve-static');
+var httpModule = require('http');
+var urlParser = require('url-parse');
+
+module.exports = function (grunt, appPath) {
+  var username = grunt.option('proxy.username'),
+      password = grunt.option('proxy.password'),
+      host = grunt.option('proxy.host');
+
+  /**
+   * This is the middleware function used by the proxy grunt-contrib-connect module.
+   */
+  return function (connect, serverOptions) {
+    if (!Array.isArray(serverOptions.base)) {
+      serverOptions.base = [serverOptions.base];
+    }
+
+    var middlewares = [
+      connect().use(
+        '/bower_components',
+        serveStatic('./bower_components')
+      ),
+      connect().use(
+        '/styles',
+        serveStatic('./target/grunt/.tmp/styles')
+      ),
+      connect().use(
+        '/modules',
+        serveStatic('app/scripts/modules')
+      ),
+      connect().use(
+        '/shared',
+        serveStatic('app/scripts/shared')
+      ),
+      connect().use(
+        '/public',
+        serveStatic('resources/public/')
+      ),
+      connect().use(
+        '/img',
+        serveStatic('app/images/')
+      ),
+      connect().use(
+        '/lib',
+        serveStatic('app/scripts/lib')
+      ),
+      connect().use(
+        '/info',
+        serveStatic('./test/app/GET/info')
+      ),
+      connect().use(
+        '/i18n',
+        serveStatic('./test/app/GET/i18n')
+      ),
+      connect().use(
+        '/sysinfo',
+        serveStatic('./test/app/GET/sysinfo')
+      )
+    ];
+
+    // Validate settings
+    var params = ['proxy.host', 'proxy.username', 'proxy.password'];
+
+    for (var id in params) {
+      if (grunt.option(params[id]) === undefined) {
+        grunt.fail.fatal('Expecting runtime parameter "' + params[id] + '". Use --' + params[id] + '=<value>');
+      }
+    }
+    // --
+
+    console.log('Proxying to ' + host);
+
+    // wraps every proxy request with digest authentication
+    httpModule.createServer(function (req, res) {
+      console.log('Proxy ' + req.method + ' ' + req.url + ' -> ' + host + req.url);
+
+      var onReadFromBackend = function (error, response, body) {
+        if (error && (typeof error != 'object' || !error.hasOwnProperty('statusCode') || !error.hasOwnProperty('body'))) {
+          throw error;
+        }
+        // forward to client
+        res.statusCode = (response || error).statusCode;
+        body = body || (error ? error.body : '');
+        res.write(body);
+        res.end();
+      };
+
+      var parsed = urlParser(req.url);
+      var parsedHost = urlParser(host);
+      var escapedQuery = parsed.query.replace(',','%2C');
+
+      var onForwardToBackend = function (body) {
+        var authConfig = {
+          host: parsedHost.protocol + '//' + parsedHost.hostname,
+          port: parsedHost.port,
+          path: parsed.pathname + escapedQuery,
+          method: req.method,
+          headers: {
+            'X-Requested-Auth': 'Digest'
+          },
+          jar: true,
+          body: body
+        };
+
+        requestDigest(username, password).request(authConfig, onReadFromBackend);
+      };
+
+      // read request buffer
+      var buffer = [];
+      req.on('data', function (chunk) {
+        buffer.push(chunk);
+      });
+      req.on('end', function () {
+        onForwardToBackend(Buffer.concat(buffer).toString());
+      });
+
+    }).listen(serverOptions.proxyPort);
+
+    //Setup the proxy routes
+    middlewares.push(proxyMiddleware);
+
+    middlewares.push(serveStatic(appPath));
+
+    return middlewares;
+  }
+}
---
+++ modules/admin-ui-frontend/test/test/unit/modules/events/controllers/bulkDeleteControllerSpec.js
@@ -0,0 +1,139 @@
+describe('Bulk Delete controller', function () {
+    var $scope, $httpBackend, bulkDeleteResource, _Notifications, successfulSubmit, table, modal, sampleRows,
+        prepareForSubmit;
+
+    // Both published and unpublished selected, workflow selected, is valid
+    sampleRows = function () {
+       return [ { id: 1, selected: true, 'publications': [{ 'id': 'engage-player', 'url': 'http://engage.localdomain' }] },
+            { id: 2, selected: true },
+            { id: 3, selected: false, 'publications': [{ 'id': 'engage-player', 'url': 'http://engage.localdomain' }] },
+            { id: 4, selected: false }];
+    };
+
+    prepareForSubmit = function () {
+        $scope.rows = sampleRows;
+    };
+
+    beforeEach(module('adminNg'));
+
+    beforeEach(module(function ($provide) {
+        bulkDeleteResource = {
+            delete: function (empty, what, success, failure) {
+                if (successfulSubmit) {
+                    success();
+                } else {
+                    failure();
+                }
+            }
+
+        };
+        $provide.value('BulkDeleteResource', bulkDeleteResource);
+
+        table = {
+            resource: 'events',
+            copySelected: function () {
+                return sampleRows();
+            },
+            getSelected: function () {
+                return sampleRows();
+            },
+            allSelectedChanged: function () {},
+            deselectAll: function () {}
+        };
+        $provide.value('Table', table);
+
+        modal = {
+            $scope: {
+                close: function () {}
+            }
+        };
+        $provide.value('Modal', modal);
+    }));
+
+    beforeEach(inject(function ($rootScope, $controller, _$httpBackend_, Notifications) {
+        $scope = $rootScope.$new();
+        $scope.close = function () {};
+        $controller('BulkDeleteCtrl', {$scope: $scope});
+        _Notifications = Notifications;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    beforeEach(function () {
+        jasmine.getJSONFixtures().fixturesPath = 'base/app/GET';
+        $httpBackend.whenGET('modules/events/partials/index.html').respond('');
+        $httpBackend.whenGET('public/languages.json').respond(JSON.stringify(getJSONFixture('i18n/languages.json')));
+        $httpBackend.whenGET('public/org/opencastproject/adminui/languages/lang-en_US.json').respond(JSON.stringify({}));
+
+        //TODO get JSON String instead of JSON Object
+    });
+
+    it('instantiates', function () {
+        expect($scope.table.rows).toBeDefined();
+        expect($scope.submit).toBeDefined();
+        expect($scope.table.allSelectedChanged).toBeDefined();
+    });
+
+    describe('#validity', function () {
+        it('is true in case everything is right', function () {
+            expect($scope.table.rows.length).toEqual(4);
+            expect($scope.valid()).toBeTruthy();
+        });
+
+        it('is false if no rows are selected', function () {
+            $scope.table.rows[0].selected = false;
+            $scope.table.rows[1].selected = false;
+            expect($scope.valid()).toBeFalsy();
+        });
+
+    });
+
+    describe('#submit', function () {
+
+        it('submits bulk delete requests', function () {
+            successfulSubmit = true;
+            spyOn(bulkDeleteResource, 'delete').and.callThrough();
+            $scope.submit();
+            var expectedArgument = {resource: 'event', endpoint: 'deleteEvents', eventIds: [1, 2]},
+                actualArguments = bulkDeleteResource.delete.calls.mostRecent();
+            expect(actualArguments.args[1]).toEqual(expectedArgument);
+        });
+
+        it('notifies the user upon success', function () {
+            successfulSubmit = true;
+            spyOn(_Notifications, 'add').and.returnValue(true);
+            $scope.submit();
+            expect(_Notifications.add).toHaveBeenCalledWith('success', 'EVENTS_DELETED');
+        });
+
+        it('notifies the user upon failure', function () {
+            successfulSubmit = false;
+            spyOn(_Notifications, 'add').and.returnValue(true);
+            $scope.submit();
+            expect(_Notifications.add).toHaveBeenCalledWith('error', 'EVENTS_NOT_DELETED');
+        });
+
+        it('closes the modal window after success', function () {
+            spyOn(modal.$scope, 'close');
+            successfulSubmit = true;
+            $scope.submit();
+            expect(modal.$scope.close).toHaveBeenCalled();
+        });
+
+        it('closes the modal window after failure', function () {
+            spyOn(modal.$scope, 'close');
+            successfulSubmit = false;
+            $scope.submit();
+            expect(modal.$scope.close).toHaveBeenCalled();
+        });
+
+        it('deselects all rows after success', function () {
+            spyOn(table, 'deselectAll');
+            prepareForSubmit();
+            successfulSubmit = true;
+            $scope.submit();
+            $httpBackend.flush();
+            expect(table.deselectAll).toHaveBeenCalled();
+        });
+    });
+
+});
