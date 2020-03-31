675db736799318b932eff5a8f389a47facca6d68
Isabella Kutger <isabella.kutger@tik.uni-stuttgart.de>
2020-03-31 10:50:05 +0200

    Initial commit; project skeleton and files that can be reused (images, styles, fonts)

---
+++ modules/admin-ui-picard/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+      //todo: change <h1> to Components and Routing stuff
+    <h1>Hello World!</h1>
+  );
+}
+
+export default App;
