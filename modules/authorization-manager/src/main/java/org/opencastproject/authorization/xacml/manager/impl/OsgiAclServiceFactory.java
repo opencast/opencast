56b188f1929731e43d09043ae7cf4a66d775811e
Katrin Ihler <ihler@elan-ev.de>
2021-03-08 10:43:06 +0100

    AclServiceFactory not needed for Index Rebuild

--- modules/authorization-manager/src/main/java/org/opencastproject/authorization/xacml/manager/impl/OsgiAclServiceFactory.java
+++ modules/authorization-manager/src/main/java/org/opencastproject/authorization/xacml/manager/impl/OsgiAclServiceFactory.java
@@ -24,29 +24,21 @@ package org.opencastproject.authorization.xacml.manager.impl;
 import org.opencastproject.assetmanager.api.AssetManager;
 import org.opencastproject.authorization.xacml.manager.api.AclService;
 import org.opencastproject.authorization.xacml.manager.api.AclServiceFactory;
-import org.opencastproject.authorization.xacml.manager.api.ManagedAcl;
 import org.opencastproject.elasticsearch.index.AbstractSearchIndex;
-import org.opencastproject.index.rebuild.AbstractIndexProducer;
-import org.opencastproject.index.rebuild.IndexRebuildService;
 import org.opencastproject.message.broker.api.MessageReceiver;
 import org.opencastproject.message.broker.api.MessageSender;
-import org.opencastproject.message.broker.api.acl.AclItem;
 import org.opencastproject.security.api.AuthorizationService;
 import org.opencastproject.security.api.Organization;
 import org.opencastproject.security.api.OrganizationDirectoryService;
 import org.opencastproject.security.api.SecurityService;
-import org.opencastproject.security.util.SecurityUtil;
 import org.opencastproject.series.api.SeriesService;
 
-import org.apache.commons.lang3.text.WordUtils;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 /** OSGi implementation of {@link org.opencastproject.authorization.xacml.manager.api.AclServiceFactory}. */
-public class OsgiAclServiceFactory extends AbstractIndexProducer implements AclServiceFactory {
+public class OsgiAclServiceFactory implements AclServiceFactory {
   /** The logger */
   private static final Logger logger = LoggerFactory.getLogger(OsgiAclServiceFactory.class);
 
@@ -121,26 +113,6 @@ public class OsgiAclServiceFactory extends AbstractIndexProducer implements AclS
     this.externalApiIndex = index;
   }
 
-  @Override
-  public void repopulate(final AbstractSearchIndex index) {
-    final String destinationId = AclItem.ACL_QUEUE_PREFIX + WordUtils.capitalize(index.getIndexName());
-    for (final Organization organization : organizationDirectoryService.getOrganizations()) {
-      SecurityUtil.runAs(securityService, organization, SecurityUtil.createSystemUser(cc, organization), () -> {
-        AclService aclService = serviceFor(organization);
-        List<ManagedAcl> acls = aclService.getAcls();
-        int total = aclService.getAcls().size();
-        logIndexRebuildBegin(logger, index.getIndexName(), total, "ACLs", organization);
-        int current = 1;
-        for (ManagedAcl acl : acls) {
-          messageSender.sendObjectMessage(destinationId, MessageSender.DestinationType.Queue,
-                  AclItem.create(acl.getName()));
-          logIndexRebuildProgress(logger, index.getIndexName(), total, current);
-          current++;
-        }
-      });
-    }
-  }
-
   /**
    * Callback for activation of this component.
    *
@@ -149,10 +121,5 @@ public class OsgiAclServiceFactory extends AbstractIndexProducer implements AclS
    */
   public void activate(ComponentContext cc) {
     this.cc = cc;
-  }
-
-  @Override
-  public IndexRebuildService.Service getService() {
-    return IndexRebuildService.Service.Acl;
   }
 }
