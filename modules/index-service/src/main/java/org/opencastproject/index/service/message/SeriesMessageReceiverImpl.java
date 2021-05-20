ba8ae949d0094c7ce95105bad72cef992f7dc184
Katrin Ihler <ihler@elan-ev.de>
2021-05-20 15:42:48 +0200

    Update series metadata in index directly

--- modules/index-service/src/main/java/org/opencastproject/index/service/message/SeriesMessageReceiverImpl.java
+++ modules/index-service/src/main/java/org/opencastproject/index/service/message/SeriesMessageReceiverImpl.java
@@ -21,28 +21,10 @@
 
 package org.opencastproject.index.service.message;
 
-import org.opencastproject.elasticsearch.api.SearchIndexException;
-import org.opencastproject.elasticsearch.api.SearchResult;
-import org.opencastproject.elasticsearch.api.SearchResultItem;
-import org.opencastproject.elasticsearch.index.event.Event;
-import org.opencastproject.elasticsearch.index.event.EventSearchQuery;
-import org.opencastproject.elasticsearch.index.series.Series;
 import org.opencastproject.message.broker.api.MessageSender;
 import org.opencastproject.message.broker.api.series.SeriesItem;
-import org.opencastproject.metadata.dublincore.DublinCore;
-import org.opencastproject.metadata.dublincore.DublinCoreCatalog;
-import org.opencastproject.metadata.dublincore.EncodingSchemeUtils;
-import org.opencastproject.security.api.User;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Optional;
-import java.util.function.Function;
 
 public class SeriesMessageReceiverImpl extends BaseMessageReceiverImpl<SeriesItem> {
-
-  private static final Logger logger = LoggerFactory.getLogger(SeriesMessageReceiverImpl.class);
 
   /**
    * Creates a new message receiver that is listening to the admin ui destination of the series queue.
@@ -53,74 +35,6 @@ public class SeriesMessageReceiverImpl extends BaseMessageReceiverImpl<SeriesIte
 
   @Override
   protected void execute(SeriesItem seriesItem) {
-    Function<Optional<Series>, Optional<Series>> updateFunction = null;
-    String organization = getSecurityService().getOrganization().getId();
-    User user = getSecurityService().getUser();
-    String seriesId = seriesItem.getSeriesId();
-
-    switch (seriesItem.getType()) {
-      case UpdateCatalog:
-        logger.debug("Received Update Series for index {}", getSearchIndex().getIndexName());
-        DublinCoreCatalog dc = seriesItem.getMetadata();
-
-        updateFunction = (Optional<Series> seriesOpt) -> {
-          Series series = seriesOpt.orElse(new Series(seriesItem.getSeriesId(), organization));
-          series.setCreator(getSecurityService().getUser().getName());
-
-          // update from dublin core catalog
-          series.setTitle(dc.getFirst(DublinCoreCatalog.PROPERTY_TITLE));
-          series.setDescription(dc.getFirst(DublinCore.PROPERTY_DESCRIPTION));
-          series.setSubject(dc.getFirst(DublinCore.PROPERTY_SUBJECT));
-          series.setLanguage(dc.getFirst(DublinCoreCatalog.PROPERTY_LANGUAGE));
-          series.setLicense(dc.getFirst(DublinCoreCatalog.PROPERTY_LICENSE));
-          series.setRightsHolder(dc.getFirst(DublinCore.PROPERTY_RIGHTS_HOLDER));
-          String createdDateStr = dc.getFirst(DublinCoreCatalog.PROPERTY_CREATED);
-          if (createdDateStr != null) {
-            series.setCreatedDateTime(EncodingSchemeUtils.decodeDate(createdDateStr));
-          }
-          series.setPublishers(dc.get(DublinCore.PROPERTY_PUBLISHER, DublinCore.LANGUAGE_ANY));
-          series.setContributors(dc.get(DublinCore.PROPERTY_CONTRIBUTOR, DublinCore.LANGUAGE_ANY));
-          series.setOrganizers(dc.get(DublinCoreCatalog.PROPERTY_CREATOR, DublinCore.LANGUAGE_ANY));
-          return Optional.of(series);
-        };
-        break;
-      case UpdateElement:
-        // nothing to do
-        break;
-      default:
-        throw new IllegalArgumentException("Unhandled type of SeriesItem");
-    }
-
-    // do the actual update
-    if (updateFunction != null) {
-      try {
-        Optional<Series> updatedSeriesOpt = getSearchIndex().addOrUpdateSeries(seriesId, updateFunction, organization,
-                user);
-
-        // update series title in events
-        if (updatedSeriesOpt.isPresent() && updatedSeriesOpt.get().isSeriesTitleUpdated()) {
-          Series updatedSeries = updatedSeriesOpt.get();
-          SearchResult<Event> events = getSearchIndex().getByQuery(
-                  new EventSearchQuery(organization, user).withoutActions().withSeriesId(updatedSeries.getIdentifier()));
-          for (SearchResultItem<Event> searchResultItem : events.getItems()) {
-            String eventId = searchResultItem.getSource().getIdentifier();
-
-            Function<Optional<Event>, Optional<Event>> eventUpdateFunction = (Optional<Event> eventOpt) -> {
-              if (eventOpt.isPresent() && eventOpt.get().getSeriesId().equals(updatedSeries.getIdentifier())) {
-                Event event = eventOpt.get();
-                event.setSeriesName(updatedSeries.getTitle());
-                return Optional.of(event);
-              }
-              return Optional.empty();
-            };
-
-            getSearchIndex().addOrUpdateEvent(eventId, eventUpdateFunction, organization, user);
-          }
-        }
-        logger.debug("Series {} updated in the search index", seriesId);
-      } catch (SearchIndexException e) {
-        logger.error("Error storing the series {} in the search index", seriesId, e);
-      }
-    }
+    return;
   }
 }
