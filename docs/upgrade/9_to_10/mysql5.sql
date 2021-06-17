a650431315e8b891f5dc800da899b48ad2151f0a
Nosipho Mali <yheyhePrec@gmail.com>
2021-06-17 14:30:46 +0200

    Changed asset mime type  length from 64 to 255

--- docs/upgrade/9_to_10/mysql5.sql
+++ docs/upgrade/9_to_10/mysql5.sql
@@ -1,6 +1,2 @@
--- Drop unused tables
-drop table oc_job_oc_service_registration;
-drop table oc_job_context;
-
 -- Increase mime_type field size
 ALTER TABLE oc_assets_asset MODIFY COLUMN mime_type VARCHAR (255);
