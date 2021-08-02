db124ca492cad5ec28eb79136db35bc82b2c8fae
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-08-02 12:30:05 +0200

    Fix checkstyle violations in module `userdirectory-brightspace`

--- modules/userdirectory-brightspace/src/main/java/org/opencastproject/userdirectory/brightspace/BrightspaceUserProviderFactory.java
+++ modules/userdirectory-brightspace/src/main/java/org/opencastproject/userdirectory/brightspace/BrightspaceUserProviderFactory.java
@@ -118,7 +118,8 @@ public class BrightspaceUserProviderFactory implements ManagedServiceFactory {
   @Override
   public void updated(String pid, Dictionary properties) throws ConfigurationException {
     logger.debug("updated BrightspaceUserProviderFactory");
-    String adminUserName = StringUtils.trimToNull(bundleContext.getProperty(SecurityConstants.GLOBAL_ADMIN_USER_PROPERTY));
+    String adminUserName = StringUtils.trimToNull(
+        bundleContext.getProperty(SecurityConstants.GLOBAL_ADMIN_USER_PROPERTY));
     String organization = (String) properties.get(ORGANIZATION_KEY);
     String urlStr = (String) properties.get(BRIGHTSPACE_URL);
     String systemUserId = (String) properties.get(BRIGHTSPACE_USER_ID);
@@ -152,9 +153,14 @@ public class BrightspaceUserProviderFactory implements ManagedServiceFactory {
     logger.debug("creating new brightspace user provider for pid={}", pid);
 
 
-    BrightspaceUserProviderInstance provider = new BrightspaceUserProviderInstance(pid,
-            new BrightspaceClientImpl(urlStr, applicationId, applicationKey, systemUserId, systemUserKey), org, cacheSize,
-            cacheExpiration, adminUserName);
+    BrightspaceUserProviderInstance provider = new BrightspaceUserProviderInstance(
+        pid,
+        new BrightspaceClientImpl(urlStr, applicationId, applicationKey, systemUserId, systemUserKey),
+        org,
+        cacheSize,
+        cacheExpiration,
+        adminUserName
+    );
     this.providerRegistrations
             .put(pid, this.bundleContext.registerService(UserProvider.class.getName(), provider, null));
     this.providerRegistrations
