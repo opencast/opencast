c826bc1a69c420162bb51e54c3dc67c403d0014a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-10-13 09:11:27 +0200

    Add `getAccessControlList` to `SearchResultItem`
    The ACL information is available in the search index in non-XML form.
    Code using the search service might be interested in that ACL without
    needing to load an attachment and parse XML. So it makes sense to
    provide this information as part of `SearchResultItem`.
    
    Unfortunately, to make the `SearchService` remote impl work (which uses
    the search API), the access control list has to be serialized and
    exposed in that API. The ACL in the index also always contains the
    organization admin role for read and write. This is also now exposed in
    the API.

--- modules/search-service-impl/src/main/java/org/opencastproject/search/impl/solr/SolrRequester.java
+++ modules/search-service-impl/src/main/java/org/opencastproject/search/impl/solr/SolrRequester.java
@@ -41,6 +41,8 @@ import org.opencastproject.search.api.SearchResultImpl;
 import org.opencastproject.search.api.SearchResultItem;
 import org.opencastproject.search.api.SearchResultItem.SearchResultItemType;
 import org.opencastproject.search.api.SearchResultItemImpl;
+import org.opencastproject.security.api.AccessControlEntry;
+import org.opencastproject.security.api.AccessControlList;
 import org.opencastproject.security.api.Role;
 import org.opencastproject.security.api.SecurityService;
 import org.opencastproject.security.api.User;
@@ -63,6 +65,7 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map.Entry;
@@ -71,6 +74,7 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Class implementing <code>LookupRequester</code> to provide connection to solr indexing facility.
@@ -209,6 +213,18 @@ public class SolrRequester {
             }
           }
           return null;
+        }
+
+        @Override
+        public AccessControlList getAccessControlList() {
+          final List<AccessControlEntry> entries = Schema.getOcAcl(doc)
+              .stream()
+              .flatMap(field -> {
+                return Arrays.stream(field.getValue().strip().split("\\s+"))
+                    .map(role -> new AccessControlEntry(role, field.getSuffix(), true));
+              })
+              .collect(Collectors.toCollection(ArrayList::new));
+          return new AccessControlList(entries);
         }
 
         @Override
