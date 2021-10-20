7eb8e12be93cf68e583583d0f5b454172f053182
Maximiliano Lira Del Canto <8040628+mliradelc@users.noreply.github.com>
2021-10-20 10:18:22 +0200

    Fix unused import

--- modules/common/src/main/java/org/opencastproject/mediapackage/MediaPackageBuilderImpl.java
+++ modules/common/src/main/java/org/opencastproject/mediapackage/MediaPackageBuilderImpl.java
@@ -31,7 +31,6 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
