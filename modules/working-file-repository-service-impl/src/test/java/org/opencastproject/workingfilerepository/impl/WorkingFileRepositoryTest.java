fdf27b44d9f5b4337278f17d6d92444aa53786ec
lwillmann <willmann@elan-ev.de>
2021-11-17 15:42:14 +0100

    Added Tests

--- modules/working-file-repository-service-impl/src/test/java/org/opencastproject/workingfilerepository/impl/WorkingFileRepositoryTest.java
+++ modules/working-file-repository-service-impl/src/test/java/org/opencastproject/workingfilerepository/impl/WorkingFileRepositoryTest.java
@@ -247,4 +247,28 @@ public class WorkingFileRepositoryTest {
     Assert.assertFalse(result);
   }
 
+  @Test
+  public void testCleanupOldFilesFromMediapackageNothingToDelete() throws Exception {
+    // Cleanup files older than 1 day, nothing should be deleted
+    boolean result = repo.cleanupOldFilesFromMediapackage(1);
+    Assert.assertTrue(result);
+    File file = null;
+    file = repo.getFile(mediaPackageID, mediaPackageElementID);
+    Assert.assertNotNull(file);
+  }
+
+  @Test
+  public void testCleanupOldFilesFromMediapackageSomethingToDelete() throws Exception {
+    // Cleanup files older than 1 day, nothing should be deleted
+    boolean result = repo.cleanupOldFilesFromMediapackage(0);
+    Assert.assertTrue(result);
+    File file = null;
+    try {
+      file = repo.getFile(mediaPackageID, mediaPackageElementID);
+      Assert.fail();
+    } catch (NotFoundException e) {
+      Assert.assertNull(file);
+    }
+  }
+
 }
