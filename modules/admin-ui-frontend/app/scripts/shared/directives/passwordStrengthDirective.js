12df3255639a69695f7a4885e098605557dac942
naweidner <weidner@elan-ev.de>
2022-01-12 11:34:28 +0100

    Password strength indicator
    Changed the colors (now red, darkorange, gold, green and blue #388ed5) and added a textual indicator.
    Tests should now run without failures.

--- modules/admin-ui-frontend/app/scripts/shared/directives/passwordStrengthDirective.js
+++ modules/admin-ui-frontend/app/scripts/shared/directives/passwordStrengthDirective.js
@@ -32,53 +32,62 @@
 
 angular.module('adminNg.directives')
     .directive('pwStrength', [function () {
-        return {
-            restrict: 'E',
-            scope: {
-                password: '=ngModel'
-            },
+      return {
+        restrict: 'E',
+        scope: {
+          password: '=ngModel'
+        },
 
-            link: function (scope, elem, attrs, ctrl) {
-                scope.$watch('password', function (pw) {
+        link: function (scope, elem, attrs, ctrl) {
+          scope.$watch('password', function (pw) {
 
-                    scope.strength =
-                        isValid(pw && /[A-Z]/.test(pw)) +
-                        isValid(pw && /[a-z]/.test(pw)) +
-                        isValid(pw && /\d/.test(pw)) +
-                        isValid(pw && /(?=.*\W)/.test(pw)) +
-                        isValid(pw && /^.{8,}$/.test(pw));
+            scope.strength =
+                isValid(pw && /[A-Z]/.test(pw)) +
+                isValid(pw && /[a-z]/.test(pw)) +
+                isValid(pw && /\d/.test(pw)) +
+                isValid(pw && /(?=.*\W)/.test(pw)) +
+                isValid(pw && /^.{8,}$/.test(pw));
 
-                    var bar = "background:white; width:0%;"
-                    setColor(scope.strength);
+            var bar = 'background:white; width:0%;';
+            var pw_strength = '';
+            setProgBar(scope.strength);
 
-                    function isValid(rule) {
-                        return rule ? 1 : 0;
-                    }
+            function isValid(rule) {
+              return rule ? 1 : 0;
+            }
 
-                    function setColor(validRules) {
-                        if (validRules == 1) {
-                            bar = "background:red; width:20%;"
-                        }
-                        else if (validRules == 2) {
-                            bar = "background:orange; width:40%;"
-                        }
-                        else if (validRules == 3) {
-                            bar = "background:yellow; width:60%;"
-                        }
-                        else if (validRules == 4) {
-                            bar = "background:green; width:80%;"
-                        }
-                        else if (validRules == 5) {
-                            bar = "background:darkgreen; width:100%;"
-                        }
-                        document.getElementById('bar').style = bar;
-                    }
-                });
+            function setProgBar(validRules) {
+              if (validRules == 1) {
+                bar = 'background:red; width:20%;';
+                pw_strength = 'Very Weak';
+              }
+              else if (validRules == 2) {
+                bar = 'background:darkorange; width:40%;';
+                pw_strength = 'Weak';
+              }
+              else if (validRules == 3) {
+                bar = 'background:gold; width:60%;';
+                pw_strength = 'Good';
+              }
+              else if (validRules == 4) {
+                bar = 'background:green; width:80%;';
+                pw_strength = 'Strong';
+              }
+              else if (validRules == 5) {
+                bar = 'background:#388ed6; width:100%;';
+                pw_strength = 'Very Strong';
+              }
+              document.getElementById('bar').style = bar;
+              document.getElementById('pw').innerHTML = pw_strength;
+            }
+          });
 
-            },
-            template: '<div class="progress pw-strength">' +
-                '<div id="bar" class="progress-bar"></div>' +
-                '</div>'
-        }
+        },
+        template:
+                '<div class="progress pw-strength">' +
+                    '<div id="bar" class="progress-bar"></div>' +
+                '</div>' +
+                '<label id="pw" style="text-align: left;">{{pw_strength}}</label>'
+      };
     }
     ]);
