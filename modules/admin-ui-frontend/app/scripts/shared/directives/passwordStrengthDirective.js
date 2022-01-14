2b1788e9e1c8f94028a9b94810debce62679662d
naweidner <weidner@elan-ev.de>
2022-01-14 16:53:55 +0100

    Password strength indicator
    Changed algorithm. Added list of bad passwords.

--- modules/admin-ui-frontend/app/scripts/shared/directives/passwordStrengthDirective.js
+++ modules/admin-ui-frontend/app/scripts/shared/directives/passwordStrengthDirective.js
@@ -41,45 +41,92 @@ angular.module('adminNg.directives')
         link: function (scope, elem, attrs, ctrl) {
           scope.$watch('password', function (pw) {
 
-            scope.strength =
-                isValid(pw && /[A-Z]/.test(pw)) +
-                isValid(pw && /[a-z]/.test(pw)) +
-                isValid(pw && /\d/.test(pw)) +
-                isValid(pw && /(?=.*\W)/.test(pw)) +
-                isValid(pw && /^.{8,}$/.test(pw));
+            scope.rule =
+                requirements(pw && /[A-Z]/.test(pw)) +
+                requirements(pw && /[a-z]/.test(pw)) +
+                requirements(pw && /\d/.test(pw)) +
+                requirements(pw && /(?=.*\W)/.test(pw)) +
+                requirements(pw && /^.{8,}$/.test(pw));
 
-            var bar = 'background:white; width:0%;';
-            var pw_strength = '';
-            setProgBar(scope.strength);
+            // bad passwords from https://en.wikipedia.org/wiki/List_of_the_most_common_passwords
+            // plus Opencast's default password
+            const bad_passwords = ['0', '111111', '1111111', '123', '123123', '123321',
+              '1234', '12345', '123456', '1234567', '12345678', '123456789', '1234567890',
+              '12345679', '123qwe', '18atcskd2w', '1q2w3e', '1q2w3e4r', '1q2w3e4r5t',
+              '3rjs1la7qe', '555555', '654321', '666666', '7777777', '888888',
+              '987654321', 'aa12345678', 'abc123', 'admin', 'dragon', 'Dragon', 'google',
+              'iloveyou', 'Iloveyou', 'lovely', 'Monkey', 'mynoob', 'password',
+              'password1', 'password12', 'password123', 'princess', 'qwerty', 'qwerty123', 'qwertyuiop',
+              'Qwertyuiop', 'welcome', 'zxcvbnm', 'opencast' ];
 
-            function isValid(rule) {
+            var bar_color = 'background:white;';
+            var bar_width = 'width:0%;';
+            var bar_text = '';
+            var strength = calcStrength(scope.rule, pw);
+
+            setProgBar(strength);
+
+            function requirements(rule) {
               return rule ? 1 : 0;
             }
 
-            function setProgBar(validRules) {
-              if (validRules == 1) {
-                bar = 'background:red; width:20%;';
-                pw_strength = 'Very Weak';
+            function calcStrength(rule, pw){
+
+              if (bad_passwords.indexOf(pw) > -1) {
+                strength = 0;
+                return strength;
               }
-              else if (validRules == 2) {
-                bar = 'background:darkorange; width:40%;';
-                pw_strength = 'Weak';
-              }
-              else if (validRules == 3) {
-                bar = 'background:gold; width:60%;';
-                pw_strength = 'Good';
-              }
-              else if (validRules == 4) {
-                bar = 'background:green; width:80%;';
-                pw_strength = 'Strong';
-              }
-              else if (validRules == 5) {
-                bar = 'background:#388ed6; width:100%;';
-                pw_strength = 'Very Strong';
-              }
-              document.getElementById('bar').style = bar;
-              document.getElementById('pw').innerHTML = pw_strength;
+              // for every unused rule/requirement = -5 points
+              var usedRules = (rule - 5) * 5;
+
+              var uniqueChars = (pw.split('').sort().join('').replace(/(.)\1+/g, '').length) * 2;
+              var pw_length = pw.length * 4;
+              var lowerCase = (pw.length - pw.replace(/[a-z]/g, '').length) * 2;
+              var upperCase = (pw.length - pw.replace(/[A-Z]/g, '').length) * 2;
+              var number = (pw.length - pw.replace(/[0-9]/g, '').length) * 4;
+              var symbol = (pw.length - pw.replace(/\W/g, '').length) * 6;
+
+              var strength = usedRules + uniqueChars + pw_length + lowerCase + upperCase + number + symbol;
+              return strength;
             }
+
+            function setProgBar(strength) {
+
+              if (strength >= 90) {
+                bar_color = 'background:#388ed6;';
+                bar_width = 'width:' + strength + '%;';
+                bar_text = 'Very strong';
+              }
+              else if (strength >= 70) {
+                bar_color = 'background:green;';
+                bar_width = 'width:' + strength + '%;';
+                bar_text = 'Strong';
+              }
+              else if (strength >= 50) {
+                bar_color = 'background:gold;';
+                bar_width = 'width:' + strength + '%;';
+                bar_text = 'Good';
+              }
+              else if (strength >= 30) {
+                bar_color = 'background:darkorange;';
+                bar_width = 'width:' + strength + '%;';
+                bar_text = 'Weak';
+              }
+              else if (strength >= 1) {
+                bar_color = 'background:red;';
+                bar_width = 'width:' + strength + '%;';
+                bar_text = 'Very weak';
+              }
+              else if (strength == 0){
+                bar_color = 'background:white;';
+                bar_width = 'width:' + 0 + '%;';
+                bar_text = 'Bad password';
+              }
+
+              document.getElementById('bar').style = bar_color + bar_width;
+              document.getElementById('pw').innerText = bar_text;
+            }
+
           });
 
         },
@@ -87,7 +134,7 @@ angular.module('adminNg.directives')
                 '<div class="progress pw-strength">' +
                     '<div id="bar" class="progress-bar"></div>' +
                 '</div>' +
-                '<label id="pw" style="text-align: left;">{{pw_strength}}</label>'
+                '<label id="pw" style="text-align: left;">{{bar_text}}</label>'
       };
     }
     ]);
