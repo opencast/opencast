1c47d3ff027904e456ce23cf99156a7b0f6b4155
naweidner <weidner@elan-ev.de>
2022-01-28 10:43:14 +0100

    Rename Files Workflow Operation Handler
    WOH for renaming files

--- modules/rename-files-workflowoperation/src/main/java/org/opencastproject/workflow/handler/rename/RenameFilesWorkflowOperationHandler.java
+++ modules/rename-files-workflowoperation/src/main/java/org/opencastproject/workflow/handler/rename/RenameFilesWorkflowOperationHandler.java
@@ -32,11 +32,17 @@ import org.opencastproject.workflow.api.WorkflowOperationHandler;
 import org.opencastproject.workflow.api.WorkflowOperationInstance;
 import org.opencastproject.workflow.api.WorkflowOperationResult;
 import org.opencastproject.workflow.api.WorkflowOperationResult.Action;
+import org.opencastproject.workspace.api.Workspace;
 
+import org.apache.commons.io.FilenameUtils;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -56,6 +62,20 @@ public class RenameFilesWorkflowOperationHandler extends AbstractWorkflowOperati
   /** The logging facility */
   private static final Logger logger = LoggerFactory.getLogger(RenameFilesWorkflowOperationHandler.class);
 
+  /** The local workspace */
+  private Workspace workspace = null;
+
+  /**
+   * Callback for declarative services configuration that will introduce us to the local workspace service.
+   * Implementation assumes that the reference is configured as being static.
+   *
+   * @param workspace
+   *          an instance of the workspace
+   */
+  public void setWorkspace(Workspace workspace) {
+    this.workspace = workspace;
+  }
+
   /**
    * {@inheritDoc}
    *
@@ -65,31 +85,64 @@ public class RenameFilesWorkflowOperationHandler extends AbstractWorkflowOperati
   @Override
   public WorkflowOperationResult start(WorkflowInstance workflowInstance, JobContext context)
           throws WorkflowOperationException {
+
     final WorkflowOperationInstance operation = workflowInstance.getCurrentOperation();
     final MediaPackage mediaPackage = workflowInstance.getMediaPackage();
 
+    logger.info("Running rename files workflow operation on workflow {}", workflowInstance.getId());
+
     // Read configuration
-    // source-flavors
-    // source-tags
+    // source-flavors source-tags
     String namePattern = operation.getConfiguration("name-pattern");
     if (namePattern == null) {
       throw new WorkflowOperationException("name-pattern must be configured");
     }
+    logger.info("name-pattern {}", namePattern);
+
 
     ConfiguredTagsAndFlavors tagsAndFlavors = getTagsAndFlavors(workflowInstance,
             Configuration.many, Configuration.many, Configuration.none, Configuration.none);
+
     // Select tracks by evaluating source tags and flavors
     List<MediaPackageElementFlavor> sourceFlavors = tagsAndFlavors.getSrcFlavors();
-    // ...
 
     for (var flavor: sourceFlavors) {
       for (var track: mediaPackage.getTracks(flavor)) {
         // rename files in tracks
+
+        var uri = track.toString();
+        logger.info("##### uri: {}", uri);
+
+        String filename = FilenameUtils.getBaseName(uri.toString());
+        String extension = FilenameUtils.getExtension(uri.toString());
+        String path = FilenameUtils.getFullPath(uri.toString());
+
+        filename = mediaPackage.getTitle().replaceAll("\\s", "_");
+        URI newUri = null;
+
+        try {
+          newUri = new URI(path + filename + "." + extension);
+          logger.info("##### newUri: {}", newUri);
+        }
+        catch (URISyntaxException e) {
+          logger.error("Error setting Filename {}", e);
+        }
+
+        track.setURI(newUri);
+
       }
     }
 
+    logger.info("Continue with unmodified media package (createResult)");
     // Continue with unmodified media package
     return createResult(mediaPackage, Action.CONTINUE);
+  }
+
+  @Activate
+  @Override
+  protected void activate(ComponentContext cc) {
+    super.activate(cc);
+    logger.info("Rename Files WOH activated.");
   }
 
 }
