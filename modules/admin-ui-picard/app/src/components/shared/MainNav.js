cea7dc86e0aecd5a15c1bb052f59eedca9f662d5
Isabella Kutger <isabella.kutger@tik.uni-stuttgart.de>
2022-02-01 13:40:54 +0100

    role check in main navigation

--- modules/admin-ui-picard/app/src/components/shared/MainNav.js
+++ modules/admin-ui-picard/app/src/components/shared/MainNav.js
@@ -3,9 +3,11 @@ import {useTranslation} from "react-i18next";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {
-    loadEventsIntoTable,
+    loadAclsIntoTable,
+    loadEventsIntoTable, loadGroupsIntoTable,
     loadJobsIntoTable,
     loadRecordingsIntoTable,
+    loadSeriesIntoTable, loadServersIntoTable, loadServicesIntoTable,
     loadThemesIntoTable,
     loadUsersIntoTable
 } from "../../thunks/tableThunks";
@@ -18,12 +20,19 @@ import {fetchStats} from "../../thunks/tableFilterThunks";
 import {setOffset} from "../../actions/tableActions";
 import {getUserInformation} from "../../selectors/userInfoSelectors";
 import {hasAccess} from "../../utils/utils";
+import {fetchSeries} from "../../thunks/seriesThunks";
+import {fetchServers} from "../../thunks/serverThunks";
+import {fetchServices} from "../../thunks/serviceThunks";
+import {fetchGroups} from "../../thunks/groupThunks";
+import {fetchAcls} from "../../thunks/aclThunks";
 
 /**
  * This component renders the main navigation that opens when the burger button is clicked
  */
-const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, loadingStats, loadingRecordings,
-                     loadingRecordingsIntoTable, loadingJobs, loadingJobsIntoTable, loadingUsers, loadingUsersIntoTable,
+const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, loadingSeries, loadingSeriesIntoTable,
+                     loadingStats, loadingRecordings, loadingRecordingsIntoTable, loadingJobs, loadingJobsIntoTable,
+                     loadingServers, loadingServersIntoTable, loadingServices, loadingServicesIntoTable,  loadingUsers,
+                     loadingUsersIntoTable, loadingGroups, loadingGroupsIntoTable, loadingAcls, loadingAclsIntoTable,
                      loadingThemes, loadingThemesIntoTable, resetOffset, user }) => {
     const { t } = useTranslation();
 
@@ -39,6 +48,17 @@ const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, lo
 
         // Load events into table
         loadingEventsIntoTable();
+    }
+
+    const loadSeries = () => {
+        // Reset the current page to first page
+        resetOffset();
+
+        // Fetching series from server
+        loadingSeries();
+
+        // Load series into table
+        loadingSeriesIntoTable();
     }
 
     const loadRecordings = () => {
@@ -63,6 +83,28 @@ const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, lo
         loadingJobsIntoTable();
     }
 
+    const loadServers = () => {
+        // Reset the current page to first page
+        resetOffset();
+
+        // Fetching servers from server
+        loadingServers();
+
+        // Load servers into table
+        loadingServersIntoTable();
+    }
+
+    const loadServices = () => {
+        // Reset the current page to first page
+        resetOffset();
+
+        // Fetching services from server
+        loadingServices();
+
+        // Load services into table
+        loadingServicesIntoTable();
+    }
+
     const loadUsers = () => {
         // Reset the current page to first page
         resetOffset();
@@ -72,6 +114,28 @@ const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, lo
 
         // Load users into table
         loadingUsersIntoTable()
+    }
+
+    const loadGroups = () => {
+        // Reset the current page to first page
+        resetOffset();
+
+        // Fetching groups from server
+        loadingGroups();
+
+        // Load groups into table
+        loadingGroupsIntoTable();
+    }
+
+    const loadAcls = () => {
+        // Reset the current page to first page
+        resetOffset();
+
+        // Fetching acls from server
+        loadingAcls();
+
+        // Load acls into table
+        loadingAclsIntoTable();
     }
 
     const loadThemes = () => {
@@ -93,31 +157,71 @@ const MainNav = ({ isOpen, toggleMenu, loadingEvents, loadingEventsIntoTable, lo
                         <div id="nav-container">
                             {/* todo: more than one href? how? roles? (see MainNav admin-ui-frontend)*/}
                             {hasAccess("ROLE_UI_NAV_RECORDINGS_VIEW", user) && (
-                                <Link to="/events/events" onClick={() => loadEvents()}>
-                                    <i className="events" title={t('NAV.EVENTS.TITLE')}/>
-                                </Link>
+                                hasAccess("ROLE_UI_EVENTS_VIEW", user) ? (
+                                    <Link to="/events/events" onClick={() => loadEvents()}>
+                                        <i className="events" title={t('NAV.EVENTS.TITLE')}/>
+                                    </Link>
+                                ) : (
+                                    hasAccess("ROLE_UI_SERIES_VIEW", user) && (
+                                        <Link to="/events/series" onClick={() => loadSeries()}>
+                                            <i className="events" title={t('NAV.EVENTS.TITLE')}/>
+                                        </Link>
+                                    )
+                                )
                             )}
-                            {hasAccess("ROLE_UI_NAV_CAPTURE_VIEW", user) && (
+                            {(hasAccess("ROLE_UI_NAV_CAPTURE_VIEW", user)
+                                && hasAccess("ROLE_UI_LOCATIONS_VIEW", user)) && (
                                 <Link to="/recordings/recordings" onClick={() => loadRecordings()}>
                                     <i className="recordings" title={t('NAV.CAPTUREAGENTS.TITLE')}/>
                                 </Link>
                             )}
                             {hasAccess("ROLE_UI_NAV_SYSTEMS_VIEW", user) && (
-                                <Link to="/systems/jobs" onClick={() => loadJobs()}>
-                                    <i className="systems" title={t('NAV.SYSTEMS.TITLE')}/>
-                                </Link>
+                                hasAccess("ROLE_UI_JOBS_VIEW", user) ? (
+                                    <Link to="/systems/jobs" onClick={() => loadJobs()}>
+                                        <i className="systems" title={t('NAV.SYSTEMS.TITLE')}/>
+                                    </Link>
+                                ) : (
+                                    hasAccess("ROLE_UI_SERVERS_VIEW", user) ? (
+                                        <Link to="/systems/servers" onClick={() => loadServers()}>
+                                            <i className="systems" title={t('NAV.SYSTEMS.TITLE')}/>
+                                        </Link>
+                                    ) : (
+                                        hasAccess("ROLE_UI_SERVICES_VIEW", user) && (
+                                            <Link to="/systems/services" onClick={() => loadServices()}>
+                                                <i className="systems" title={t('NAV.SYSTEMS.TITLE')}/>
+                                            </Link>
+                                        )
+                                    )
+                                )
                             )}
                             {hasAccess("ROLE_UI_NAV_ORGANIZATION_VIEW", user) && (
-                                <Link to="/users/users" onClick={() => loadUsers()}>
-                                    <i className="users" title={t('NAV.USERS.TITLE')}/>
-                                </Link>
+                                hasAccess("ROLE_UI_USERS_VIEW", user) ? (
+                                    <Link to="/users/users" onClick={() => loadUsers()}>
+                                        <i className="users" title={t('NAV.USERS.TITLE')}/>
+                                    </Link>
+                                ) : (
+                                    hasAccess("ROLE_UI_GROUPS_VIEW", user) ? (
+                                        <Link to="/users/groups" onClick={() => loadGroups()}>
+                                            <i className="users" title={t('NAV.USERS.TITLE')}/>
+                                        </Link>
+                                    ) : (
+                                        hasAccess("ROLE_UI_ACLS_VIEW", user) && (
+                                            <Link to="/users/acls" onClick={() => loadAcls()}>
+                                                <i className="users" title={t('NAV.USERS.TITLE')}/>
+                                            </Link>
+                                        )
+                                    )
+                                )
+
                             )}
-                            {hasAccess("ROLE_UI_NAV_CONFIGURATION_VIEW", user) && (
+                            {(hasAccess("ROLE_UI_NAV_CONFIGURATION_VIEW", user)
+                                && hasAccess("ROLE_UI_THEMES_VIEW", user)) && (
                                 <Link to="/configuration/themes" onClick={() => loadThemes()}>
                                     <i className="configuration" title={t('NAV.CONFIGURATION.TITLE')}/>
                                 </Link>
                             )}
-                            {hasAccess("ROLE_UI_NAV_STATISTICS_VIEW", user) && (
+                            {(hasAccess("ROLE_UI_NAV_STATISTICS_VIEW", user)
+                                && hasAccess("ROLE_UI_STATISTICS_ORGANIZATION_VIEW", user)) && (
                                 <Link to="/statistics/organization">
                                     <i className="statistics" title={t('NAV.STATISTICS.TITLE')}/>
                                 </Link>
@@ -140,13 +244,23 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     loadingEvents: () => dispatch(fetchEvents()),
     loadingEventsIntoTable: () => dispatch(loadEventsIntoTable()),
+    loadingSeries: () => dispatch(fetchSeries()),
+    loadingSeriesIntoTable: () => dispatch(loadSeriesIntoTable()),
     loadingStats: () => dispatch(fetchStats()),
     loadingRecordings: () => dispatch(fetchRecordings()),
     loadingRecordingsIntoTable: () => dispatch(loadRecordingsIntoTable()),
     loadingJobs: () => dispatch(fetchJobs()),
     loadingJobsIntoTable: () => dispatch(loadJobsIntoTable()),
+    loadingServers: () => dispatch(fetchServers()),
+    loadingServersIntoTable: () => dispatch(loadServersIntoTable()),
+    loadingServices: () => dispatch(fetchServices()),
+    loadingServicesIntoTable: () => dispatch(loadServicesIntoTable()),
     loadingUsers: () => dispatch(fetchUsers()),
     loadingUsersIntoTable: () => dispatch(loadUsersIntoTable()),
+    loadingGroups: () => dispatch(fetchGroups()),
+    loadingGroupsIntoTable: () => dispatch(loadGroupsIntoTable()),
+    loadingAcls: () => dispatch(fetchAcls()),
+    loadingAclsIntoTable: () => dispatch(loadAclsIntoTable()),
     loadingThemes: () => dispatch(fetchThemes()),
     loadingThemesIntoTable: () => dispatch(loadThemesIntoTable()),
     resetOffset: () => dispatch(setOffset(0))
