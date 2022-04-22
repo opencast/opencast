0a02f0b4c5bf562c49daeda72f0036e48eb52dd5
Arnei <arnewilken@yahoo.de>
2022-04-22 09:22:52 +0200

    Remove unused imports

--- modules/external-api/src/main/java/org/opencastproject/external/endpoint/SeriesEndpoint.java
+++ modules/external-api/src/main/java/org/opencastproject/external/endpoint/SeriesEndpoint.java
@@ -121,7 +121,6 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.HEAD;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
--- modules/series-service-impl/src/main/java/org/opencastproject/series/impl/SeriesServiceImpl.java
+++ modules/series-service-impl/src/main/java/org/opencastproject/series/impl/SeriesServiceImpl.java
@@ -78,7 +78,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
