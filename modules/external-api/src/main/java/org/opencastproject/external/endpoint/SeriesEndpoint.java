db04b161600563b969c77f8ad7496528e387cd69
Maximiliano Lira Del Canto <8040628+mliradelc@users.noreply.github.com>
2022-08-17 14:12:22 +0200

    Improve malformed datetime error message

--- modules/external-api/src/main/java/org/opencastproject/external/endpoint/SeriesEndpoint.java
+++ modules/external-api/src/main/java/org/opencastproject/external/endpoint/SeriesEndpoint.java
@@ -238,8 +238,11 @@ public class SeriesEndpoint {
               } catch (IllegalArgumentException e) {
                 return RestUtil.R.badRequest(e.getMessage());
               } catch (ArrayIndexOutOfBoundsException e) {
-                logger.error("Malformed date period. Correct UTC time period format: yyyy-MM-ddTHH:mm:ssZ/yyyy-MM-ddTHH:mm:ssZ");
-                return RestUtil.R.badRequest(e.getMessage());
+                String dateErrorMsg = String.format("Filter Series API error: Malformed date period. "
+                    + "Correct UTC time period format: yyyy-MM-ddTHH:mm:ssZ/yyyy-MM-ddTHH:mm:ssZ, "
+                    + "stated date period string: \"%s\"", value);
+                logger.warn(dateErrorMsg);
+                return RestUtil.R.badRequest(dateErrorMsg);
               }
           } else if ("Creator".equals(name)) {
             query.withCreator(value);
