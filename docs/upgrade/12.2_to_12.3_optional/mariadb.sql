8aabf031df49e2688d46933822afbaef851c57ce
Matthias Neugebauer <matthias.neugebauer@uni-muenster.de>
2022-09-19 21:59:07 +0200

    Increase string DB data types for new workflow tables
    Workflow execution could fail hard if a workflow or workflow operation
    would use a long description or if condition (more than 255 characters).
    This increases both fields.

--- docs/upgrade/12.2_to_12.3_optional/mariadb.sql
+++ docs/upgrade/12.2_to_12.3_optional/mariadb.sql
@@ -1,2 +1,9 @@
 CREATE INDEX IF NOT EXISTS IX_oc_job_argument_id ON oc_job_argument (id);
 CREATE INDEX IF NOT EXISTS IX_oc_job_argument_id_argument ON oc_job_argument (id, argument);
+
+ALTER TABLE oc_workflow
+  MODIFY COLUMN `description` LONGTEXT DEFAULT NULL;
+
+ALTER TABLE oc_workflow_operation
+  MODIFY COLUMN `description` LONGTEXT DEFAULT NULL,
+  MODIFY COLUMN `if_condition` VARCHAR(65535) DEFAULT NULL;
--- docs/upgrade/12.2_to_12.3_optional/postgresql.sql
+++ docs/upgrade/12.2_to_12.3_optional/postgresql.sql
@@ -1,2 +1,9 @@
 CREATE INDEX IF NOT EXISTS IX_oc_job_argument_id ON oc_job_argument (id);
 CREATE INDEX IF NOT EXISTS IX_oc_job_argument_id_argument ON oc_job_argument (id, argument);
+
+ALTER TABLE oc_workflow
+  ALTER COLUMN description TYPE TEXT;
+
+ALTER TABLE oc_workflow_operation
+  ALTER COLUMN description TYPE TEXT,
+  ALTER COLUMN if_condition TYPE VARCHAR(65535);
