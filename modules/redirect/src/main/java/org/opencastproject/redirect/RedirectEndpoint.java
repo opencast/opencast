4c88b1776080f63285230cef65d00ce8af841b31
Julian Kniephoff <kniephoff@elan-ev.de>
2022-11-23 15:39:38 +0100

    Add a module/endpoint/service to redirect users to a given URL
    This seems useless at first sight, but enables certain patterns
    that are specifically useful for external applications.
    
    An example of this is (pre-)authentication of users
    for Opencast services by external applications using a JWT,
    without using third party cookies, browsers' local storage,
    and also avoiding having the JWT be part of the target URL:
    You would have a `form` with `<input type="hidden">` fields
    containing the URL of an Opencast service (e.g. Opencast Studio)
    and a JWT.
    The `action` of the form would be `<opencast>/redirect/get`,
    and the method would be `POST`.
    On submitting this form, the request would go through the usual
    Spring Security mechanisms, giving you a session based on the JWT
    (picked up by the `JWTRequestParameterFilter`) and then
    redirecting you to the target URL with that session.
    
    This seems a bit roundabout, but I invite everyone to think through
    why every piece of this scheme is necessary. Specifically, you can't
    use `fetch` (or old style AJAX), because that's not a navigation,
    running into the whole third party cookie mine field.

---
+++ modules/redirect/src/main/java/org/opencastproject/redirect/RedirectEndpoint.java
@@ -0,0 +1,102 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.opencastproject.redirect;
+
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_SEE_OTHER;
+import static org.opencastproject.util.doc.rest.RestParameter.Type.STRING;
+
+import org.opencastproject.util.doc.rest.RestParameter;
+import org.opencastproject.util.doc.rest.RestQuery;
+import org.opencastproject.util.doc.rest.RestResponse;
+import org.opencastproject.util.doc.rest.RestService;
+
+import org.osgi.service.component.annotations.Component;
+
+import java.net.URI;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * Endpoints for redirection schemes.
+ *
+ * Useful for example to enable external applications to implement certain authentication schemes.
+ */
+@Component(
+    service = RedirectEndpoint.class,
+    immediate = true,
+    property = {
+        "service.description=Redirection REST Endpoints",
+        "opencast.service.type=org.opencastproject.redirect",
+        "opencast.service.path=/redirect",
+        "opencast.service.jobproducer=false"
+    }
+)
+@RestService(
+    name = "Redirect",
+    title = "Redirection Service",
+    abstractText = "This service redirects to given URLs.",
+    notes = {
+        "This is mostly useful for external applications, for example to realize certain (pre-)authentication schemes.."
+    }
+)
+@Path("/")
+public class RedirectEndpoint {
+  /**
+   * Essentially the Post/Redirect/Get pattern
+   *
+   * @see <a href="https://en.wikipedia.org/wiki/Post/Redirect/Get">Wikipedia</a>
+   */
+  @RestQuery(
+      name = "get",
+      description = "Redirects to the given `target` URL specifically instructing the client to issue a `GET` request.",
+      restParameters = {
+          @RestParameter(name = "target", description = "The URL to redirect to", isRequired = true, type = STRING)
+      },
+      responses = {
+          @RestResponse(description = "successful redirect", responseCode = SC_SEE_OTHER),
+          @RestResponse(description = "missing or invalid target URL", responseCode = SC_BAD_REQUEST)
+      },
+      returnDescription = "A temporary redirect to the given `target` URL"
+  )
+  @POST
+  @Path("get")
+  public Response get(@FormParam("target") String target) {
+    if (target == null) {
+      throw new WebApplicationException("missing `target` URL", Status.BAD_REQUEST);
+    }
+
+    URI targetUri;
+    try {
+      targetUri = URI.create(target);
+    } catch (IllegalArgumentException e) {
+      throw new WebApplicationException("invalid `target` URL", e, Status.BAD_REQUEST);
+    }
+
+    return Response.seeOther(URI.create(target)).build();
+  }
+}
---
+++ modules/redirect/src/test/java/org/opencastproject/redirect/RedirectEndpointTest.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.opencastproject.redirect;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+/** Tests for the redirection endpoints */
+public class RedirectEndpointTest {
+  /** The endpoint under test */
+  private final RedirectEndpoint endpoint = new RedirectEndpoint();
+
+  /** Test the `POST /redirect/get` endpoint */
+  @Test
+  public void testPostRedirectGet() {
+    String url = "https://localhost:8080/studio";
+    Response response = endpoint.get(url);
+    Assert.assertEquals(response.getStatus(), 303);
+    Assert.assertEquals(response.getLocation().toString(), url);
+  }
+
+  /** Test `POST /redirect/get` with missing target */
+  @Test
+  public void testPostRedirectGetMissingTarget() {
+    WebApplicationException exception = Assert.assertThrows(
+            WebApplicationException.class,
+            () -> endpoint.get(null));
+    Assert.assertEquals(exception.getResponse().getStatus(), 400);
+  }
+
+  /** Test `POST /redirect/get` with invalid target */
+  @Test
+  public void testPostRedirectGetInvalidTarget() {
+    WebApplicationException exception = Assert.assertThrows(
+            WebApplicationException.class,
+            () -> endpoint.get("this is not a valid URL"));
+    Assert.assertEquals(exception.getResponse().getStatus(), 400);
+  }
+}
