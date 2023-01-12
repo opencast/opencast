opencast/opencast

commit eb85d3e2c2c8040b402170fef08f352a21c73fa4

Removing commented out unused code

modules/distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
@@ -560,8 +560,6 @@ public class DownloadDistributionServiceImpl extends AbstractDistributionService
     MediaPackageElement[] distributedElements = distributeElements(channelId, mediapackage, elementIds,
             checkAvailability, preserveReference);
     return Arrays.asList(distributedElements);
-//    return (distributedElements != null)
-//            ? MediaPackageElementParser.getArrayAsXml(Arrays.asList(distributedElements)) : null;
   }
 
   @Override
@@ -602,31 +600,6 @@ public class DownloadDistributionServiceImpl extends AbstractDistributionService
     notNull(channelId, "channelId");
     MediaPackageElement[] retractedElements = retractElements(channelId, mediapackage, elementIds);
     return Arrays.asList(retractedElements);
-//    return (retractedElements != null) ? MediaPackageElementParser.getArrayAsXml(Arrays.asList(retractedElements))
-//            : null;
-  }
-
-//  @Override
-//  public List<MediaPackageElement> retractSync(String channelId, MediaPackage mediaPackage, Set<String> elementIds)
-//          throws DistributionException {
-//    Job job = null;
-//    try {
-//      job = serviceRegistry
-//          .createJob(
-//              JOB_TYPE, Operation.Retract.toString(), null, null, false, retractJobLoad);
-//      job.setStatus(Job.Status.RUNNING);
-//      job = serviceRegistry.updateJob(job);
-//      final MediaPackageElement[] mediaPackageElements = this.retractElements(channelId, mediaPackage, elementIds);
-//      job.setStatus(Job.Status.FINISHED);
-//      return Arrays.asList(mediaPackageElements);
-//    } catch (ServiceRegistryException e) {
-//      throw new DistributionException(e);
-//    } catch (NotFoundException e) {
-//      throw new DistributionException("Unable to update retraction job", e);
-//    } finally {
-//      finallyUpdateJob(job);
-//    }
-//  }
 
   /**
    * Retract a media package element from the distribution channel. The retracted element must not necessarily be the

commit 25603fbd65675a2b14675c53378ab588e7934e25

Fix Syntax Error
Commit eb85d3e broke the build of Opencast 13 since it did not only
remove comments, but also syntax elements.

modules/distribution-service-download/src/main/java/org/opencastproject/distribution/download/DownloadDistributionServiceImpl.java
@@ -600,6 +600,7 @@ public class DownloadDistributionServiceImpl extends AbstractDistributionService
     notNull(channelId, "channelId");
     MediaPackageElement[] retractedElements = retractElements(channelId, mediapackage, elementIds);
     return Arrays.asList(retractedElements);
+  }
 
   /**
    * Retract a media package element from the distribution channel. The retracted element must not necessarily be the