01cad161d937eb6dab28d95f55868c86bb5ed011
Miguel Escriva <miesgre@gmail.com>
2023-03-29 10:10:53 +0200

    Paella 7: Allow to disable the cookie consent banner (#4782)
    * Allow to disable the cookie consent banner
    
    * Fix linter
    
    * Update config.json
    
    Disable cookie consent by default / Cookie Consent now using local JS instead calling a third party

--- modules/engage-paella-player-7/src/index.js
+++ modules/engage-paella-player-7/src/index.js
@@ -110,6 +110,22 @@ const initParams = {
       }
     };
 
+    // check cookie consent (if enabled)
+    const cookieConsent = config?.opencast?.cookieConsent?.enable ?? true;
+    const cookieConsentConfig = config?.opencast?.cookieConsent?.config ?? {
+      'notice_banner_type':'headline',
+      'consent_type':'express',
+      'palette':'dark',
+      'language':'en',
+      'page_load_consent_levels':['strictly-necessary'],
+      'notice_banner_reject_button_hide':false,
+      'preferences_center_close_button_hide':false,
+      'page_refresh_confirmation_buttons':false,
+      'website_name': 'Paella - opencast player'
+    };
+    if (cookieConsent == true) {
+      cookieconsent.run(cookieConsentConfig);
+    }
     // Load episode
     const data = await loadEpisode();
     if (data === null) {
