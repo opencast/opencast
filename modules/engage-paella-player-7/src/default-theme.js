daaf7913b0998d2acdbf264dd4b32f4e63195499
Miguel Escriva <miesgre@gmail.com>
2023-06-05 09:41:27 +0200

    Default opencast theme updated to use skin feature (better maintainability).

--- modules/engage-paella-player-7/src/default-theme.js
+++ modules/engage-paella-player-7/src/default-theme.js
@@ -19,72 +19,10 @@
  *
  */
 
-import { utils } from 'paella-core';
-// Customized icons
-import BackwardIcon from './icons/backward-icon.svg';
-import ForwardIcon from './icons/forward-icon.svg';
-import CaptionsIcon from './icons/captions-icon.svg';
-import CloseIcon from './icons/close.svg';
-import FullscreenIcon from './icons/fullscreen-icon.svg';
-import FullscreenExitIcon from './icons/fullscreen-exit.svg';
-import MaximizeIcon from './icons/maximize.svg';
-import MinimizeIcon from './icons/minimize.svg';
-import PlayIcon from './icons/play-icon.svg';
-import NextIcon from './icons/slide-next-icon.svg';
-import PrevIcon from './icons/slide-prev-icon.svg';
-import SlidesIcon from './icons/slides-icon.svg';
-import ViewModeIcon from './icons/view-mode.svg';
-import VolumeHighIcon from './icons/volume-base-icon.svg';
-import VolumeMidIcon from './icons/volume-mid-icon.svg';
-import VolumeLowIcon from './icons/volume-low-icon.svg';
-import VolumeMuteIcon from './icons/volume-mute-icon.svg';
-import { getUrlFromOpencastConfig } from './js/PaellaOpencast';
-
-
-async function applyDefaultTheme(paella) {
-  await utils.loadStyle('style.css');
-
-  //// Customized icons
-  //// fullscreen
-  await paella.addCustomPluginIcon('es.upv.paella.fullscreenButton','fullscreenIcon',FullscreenIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.fullscreenButton','windowedIcon', FullscreenExitIcon);
-
-  //// volume
-  await paella.addCustomPluginIcon('es.upv.paella.volumeButtonPlugin','volumeHighIcon',VolumeHighIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.volumeButtonPlugin','volumeMidIcon',VolumeMidIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.volumeButtonPlugin','volumeLowIcon',VolumeLowIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.volumeButtonPlugin','volumeMuteIcon',VolumeMuteIcon);
-
-  //// layout icons
-  await paella.addCustomPluginIcon('es.upv.paella.dualVideoDynamic','iconMaximize', MaximizeIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.dualVideoDynamic','iconMimimize', MinimizeIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.dualVideoDynamic','iconClose', CloseIcon);
-  //// play button
-  await paella.addCustomPluginIcon('es.upv.paella.playPauseButton','play', PlayIcon);
-
-  //// layout selector
-  await paella.addCustomPluginIcon('es.upv.paella.layoutSelector','layoutIcon', ViewModeIcon);
-
-  //// backward 30 segonds
-  await paella.addCustomPluginIcon('es.upv.paella.backwardButtonPlugin','backwardIcon', BackwardIcon);
-  //// forward 30 segonds
-  await paella.addCustomPluginIcon('es.upv.paella.forwardButtonPlugin','forwardIcon', ForwardIcon);
-
-  //// captions icon
-  await paella.addCustomPluginIcon('es.upv.paella.captionsSelectorPlugin','captionsIcon',CaptionsIcon);
-
-  //// slides icon
-  await paella.addCustomPluginIcon('es.upv.paella.frameControlButtonPlugin','photoIcon',SlidesIcon);
-
-  //// slide navigation
-  await paella.addCustomPluginIcon('es.upv.paella.nextSlideNavigatorButton','arrowRightIcon', NextIcon);
-  await paella.addCustomPluginIcon('es.upv.paella.prevSlideNavigatorButton','arrowLeftIcon', PrevIcon);
-}
+import { getUrlFromOpencastConfig, getUrlFromOpencastPaella } from './js/PaellaOpencast';
 
 
 export async function applyOpencastTheme(paella) {
-  await applyDefaultTheme(paella);
-
   //// Load custom theme
   const config = await fetch(getUrlFromOpencastConfig('config.json'));
   const configJson = await config.json();
@@ -94,9 +32,11 @@ export async function applyOpencastTheme(paella) {
     ?? configJson?.opencast?.theme
     ?? 'default_theme'  ;
   try {
+    paella.log.info(`Applying opencast theme '${ocTheme}'.`);
     await paella.skin.loadSkin(getUrlFromOpencastConfig(`${ocTheme}/theme.json`));
   }
   catch (err) {
     paella.log.info(`Error applying opencast theme '${ocTheme}'. Using default theme!`);
+    await paella.skin.loadSkin(getUrlFromOpencastPaella('default_theme/theme.json'));
   }
 }
