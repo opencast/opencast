e8d7e68ccffaba9ae553a7d27005c02d5e6ca811
Greg Logan <gregorydlogan@gmail.com>
2023-09-06 11:53:54 -0600

    If tha mediapackage is in the index, but *not* in the db (the source of truth), drop it

--- modules/workflow-service-impl/src/main/java/org/opencastproject/workflow/impl/WorkflowServiceImpl.java
+++ modules/workflow-service-impl/src/main/java/org/opencastproject/workflow/impl/WorkflowServiceImpl.java
@@ -31,6 +31,7 @@ import static org.opencastproject.workflow.api.WorkflowInstance.WorkflowState.ST
 import static org.opencastproject.workflow.api.WorkflowInstance.WorkflowState.SUCCEEDED;
 
 import org.opencastproject.assetmanager.api.AssetManager;
+import org.opencastproject.assetmanager.api.query.RichAResult;
 import org.opencastproject.assetmanager.util.WorkflowPropertiesUtil;
 import org.opencastproject.elasticsearch.api.SearchIndexException;
 import org.opencastproject.elasticsearch.api.SearchResult;
@@ -2200,9 +2201,14 @@ public class WorkflowServiceImpl extends AbstractIndexProducer implements Workfl
 
               String orgid = indexData.getOrganizationId();
               if (null == orgid) {
+                String mpId = indexData.getMediaPackageId();
                 //We're assuming here that mediapackages don't change orgs
-                orgid = assetManager.getSnapshotsById(indexData.getMediaPackageId())
-                    .getSnapshots().head2().getOrganizationId();
+                RichAResult results = assetManager.getSnapshotsById(mpId);
+                if (results.getSize() == 0) {
+                  logger.debug("Dropping {} from the index since it is missing from the database", mpId);
+                  continue;
+                }
+                orgid = results.getSnapshots().head2().getOrganizationId();
                 //We try-catch here since it's possible for the WF to exist in the *index* but not in the *DB*
                 // It probably shouldn't be, but that won't keep it from happening anyway.
                 try {
