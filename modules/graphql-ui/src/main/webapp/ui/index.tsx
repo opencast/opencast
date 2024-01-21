6587c55b47b88f92126b05e32c3b65c0907b67f0
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2024-01-21 23:40:39 +0100

    Configure eslint and fix errors

--- modules/graphql-ui/src/main/webapp/ui/index.tsx
+++ modules/graphql-ui/src/main/webapp/ui/index.tsx
@@ -1,37 +1,43 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import {GraphiQL} from "graphiql";
+import { createRoot } from "react-dom/client";
+import { GraphiQL } from "graphiql";
 import { explorerPlugin } from "@graphiql/plugin-explorer";
-import {createLocalStorage, Fetcher} from "@graphiql/toolkit";
+import { createLocalStorage, Fetcher } from "@graphiql/toolkit";
 import "graphiql/graphiql.css";
 import "@graphiql/plugin-explorer/dist/style.css";
 
 import "./index.css";
 
 const App: React.FC = () => {
-    const fetcher: Fetcher = async graphQLParams => {
-        const data = await fetch(
-            '/graphql',
-            {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(graphQLParams),
-                credentials: 'same-origin',
-            },
-        );
-        return data.json().catch(() => data.text());
-    };
-
-    return (
-        <GraphiQL
-            plugins={[explorerPlugin({})]}
-            fetcher={fetcher}
-            storage={createLocalStorage({namespace: "opencast"})}
-            shouldPersistHeaders></GraphiQL>
+  const fetcher: Fetcher = async graphQLParams => {
+    const data = await fetch(
+      "/graphql",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(graphQLParams),
+        credentials: "same-origin",
+      },
     );
+    return data.json().catch(() => data.text());
+  };
+
+  return (
+    <GraphiQL
+      plugins={[explorerPlugin({})]}
+      fetcher={fetcher}
+      storage={createLocalStorage({ namespace: "opencast" })}
+      shouldPersistHeaders></GraphiQL>
+  );
 };
-const root = createRoot(document.getElementById('root')!);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw new Error("No root element found");
+}
+
+const root = createRoot(rootElement);
 root.render(<App />);
