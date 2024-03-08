537102595f0ca744cdc50a1dc0aa884ae7a8eae4
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2024-03-08 19:35:27 +0100

    Fixed NPE on filtering data
    Systems with at least one role without a description (value=null) will throw a NullPointerException when calling the REST endpoint /admin-ng/acl/roles.json?target=ACL&query=foo with query argument set. This issue is addressed in some modules, but not averywhere. I hope I found them all.

--- modules/external-api/src/main/java/org/opencastproject/external/userdirectory/ExternalApiRoleProvider.java
+++ modules/external-api/src/main/java/org/opencastproject/external/userdirectory/ExternalApiRoleProvider.java
@@ -130,6 +130,9 @@ public class ExternalApiRoleProvider implements RoleProvider {
   }
 
   private static boolean like(String string, final String query) {
+    if (string == null) {
+      return false;
+    }
     String regex = query.replace("_", ".").replace("%", ".*?");
     Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
     return p.matcher(string).matches();
