0ba7b05de51acc7e96157259e20b95ded45dd2d5
Arne Wilken <Arnei@users.noreply.github.com>
2024-03-20 10:58:47 +0100

    Update CascadeType for Playlist ACL Entries
    Co-authored-by: geichelberger <35195803+geichelberger@users.noreply.github.com>

--- modules/playlists/src/main/java/org/opencastproject/playlists/Playlist.java
+++ modules/playlists/src/main/java/org/opencastproject/playlists/Playlist.java
@@ -110,7 +110,7 @@ public class Playlist {
 
   @OneToMany(
       fetch = FetchType.LAZY,
-      cascade = { CascadeType.PERSIST, CascadeType.REMOVE },
+      cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE },
       mappedBy = "playlist",
       orphanRemoval = true
   )
