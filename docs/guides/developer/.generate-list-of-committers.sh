0865aa3b2ef53519ec56a3815621481beb92a653
Lars Kiesow <lkiesow@uos.de>
2024-07-31 17:34:07 +0200

    Auto-generate list of committers
    This patch auto-generates the current list of committers based on the
    committers team in GitHuib, so that we do not have to maintain this
    manually any longer.

--- docs/guides/developer/.generate-list-of-committers.sh
+++ docs/guides/developer/.generate-list-of-committers.sh
@@ -3,23 +3,28 @@
 set -eu
 
 # https://github.com/settings/tokens
-USER=<username>
-TOKEN=<personal-github-access-token>
+#GITHUB_USER=<username>
+#GITHUB_PA_TOKEN=<personal-github-access-token>
 
-echo 'List of Committers'
-echo '------------------'
-echo
-echo '<ul>'
+OUT=docs/participate/list-of-committers.md.include
+
+echo 'List of Committers' > "${OUT}"
+echo '------------------' >> "${OUT}"
+echo >> "${OUT}"
+echo 'The current list of committers in aplhabetical order:' >> "${OUT}"
+echo >> "${OUT}"
+echo '<ul>' >> "${OUT}"
 
 LINK='<a href=" + .html_url + ">'
 IMG='<img style=\"width: 40px; margin: 0\" src=" + .avatar_url + " /> "'
-curl -s -u "$USER:$TOKEN" \
+curl -s -u "$GITHUB_USER:$GITHUB_PA_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/orgs/opencast/teams/committers/members \
     | jq -r '.[] | "<li>'"${LINK}${IMG}"' + .login + "</a></li>"' \
     | sort -f \
     | grep -v oc-bot \
     | sed 's_margin: 0"_margin: 0"\n_g' \
-    | sed 's_ />_\n />_g'
+    | sed 's_ />_\n />_g' \
+    >> "${OUT}"
 
-echo '</ul>'
+echo '</ul>' >> "${OUT}"
