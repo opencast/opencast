2497e50fb8ba4b5bacde09095e976739d09ac17f
Lars Kiesow <lkiesow@uos.de>
2024-10-09 00:09:21 +0200

    Allow rolling back to a specified snapshot in asset manager
    This patch improves the `asset-delete` workflow operation handlöer to
    not only allow for deleting all or all except the latest snapshot of an
    event, but also allows deleting all version newer than a given version.
    This essentially means that you can roll back to a given event in the
    asset manager.
    
    Along the way, this also fixes the problem of this operation creating
    errors since it may delete files the current workflow requires to work
    on. This could previously be worked around using `ingest-download`, but
    is actually quite unnecessary.

--- modules/asset-manager-workflowoperation/src/main/java/org/opencastproject/workflow/handler/assetmanager/AssetManagerDeleteWorkflowOperationHandler.java
+++ modules/asset-manager-workflowoperation/src/main/java/org/opencastproject/workflow/handler/assetmanager/AssetManagerDeleteWorkflowOperationHandler.java
@@ -21,9 +21,13 @@
 package org.opencastproject.workflow.handler.assetmanager;
 
 import static org.opencastproject.assetmanager.api.AssetManager.DEFAULT_OWNER;
+import static org.opencastproject.mediapackage.MediaPackageElements.EPISODE;
+import static org.opencastproject.mediapackage.MediaPackageElements.XACML_POLICY_EPISODE;
+import static org.opencastproject.mediapackage.MediaPackageElements.XACML_POLICY_SERIES;
 
 import org.opencastproject.assetmanager.api.AssetManager;
 import org.opencastproject.assetmanager.api.query.AQueryBuilder;
+import org.opencastproject.assetmanager.impl.VersionImpl;
 import org.opencastproject.job.api.JobContext;
 import org.opencastproject.mediapackage.MediaPackage;
 import org.opencastproject.serviceregistry.api.ServiceRegistry;
@@ -34,14 +38,19 @@ import org.opencastproject.workflow.api.WorkflowOperationHandler;
 import org.opencastproject.workflow.api.WorkflowOperationInstance;
 import org.opencastproject.workflow.api.WorkflowOperationResult;
 import org.opencastproject.workflow.api.WorkflowOperationResult.Action;
+import org.opencastproject.workspace.api.Workspace;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * Workflow operation for deleting an episode from the asset manager.
@@ -62,8 +71,12 @@ public class AssetManagerDeleteWorkflowOperationHandler extends AbstractWorkflow
   /** The archive */
   private AssetManager assetManager;
 
+  /** The workspace */
+  private Workspace workspace;
+
   /** Configuration if last snapshot should not be deleted */
   private static final String OPT_LAST_SNAPSHOT = "keep-last-snapshot";
+  private static final String OPT_ROLL_BACK_TO = "roll-back-to";
 
   @Activate
   @Override
@@ -77,6 +90,11 @@ public class AssetManagerDeleteWorkflowOperationHandler extends AbstractWorkflow
     this.assetManager = assetManager;
   }
 
+  @Reference
+  public void setWorkspace(Workspace workspace) {
+    this.workspace = workspace;
+  }
+
   @Override
   public WorkflowOperationResult start(WorkflowInstance workflowInstance, JobContext context)
           throws WorkflowOperationException {
@@ -85,15 +103,42 @@ public class AssetManagerDeleteWorkflowOperationHandler extends AbstractWorkflow
 
     WorkflowOperationInstance currentOperation = workflowInstance.getCurrentOperation();
     boolean keepLastSnapshot = BooleanUtils.toBoolean(currentOperation.getConfiguration(OPT_LAST_SNAPSHOT));
+    long rollbackVersion = NumberUtils.toLong(currentOperation.getConfiguration(OPT_ROLL_BACK_TO), -1);
+
+    if (rollbackVersion >= 0 && keepLastSnapshot) {
+      throw new WorkflowOperationException("Operation cannot roll back to an old version and keep the latest version.");
+    }
 
     try {
       final AQueryBuilder q = assetManager.createQuery();
       final long deleted;
 
+      // Unless keepLatestSnapshot is true, we will probably delete files we are working on.
+      // This leads to errors like this in the subsequent workflow execution:
+      //   ERROR | (DublinCoreUtil:79) - Unable to load metadata from catalog
+      //   'http://localhost:8080/assets/assets/01/.../4/dublincore.xml
+      //   org.opencastproject.util.NotFoundException: null
+      // To avoid this, we load the necessary files into the working file repository.
+      // Necessary files are the episode metadata catalog and the ACLs.
+      if (!keepLastSnapshot) {
+        for (var flavor: List.of(EPISODE, XACML_POLICY_EPISODE, XACML_POLICY_SERIES)) {
+          for (var element : mediaPackage.getElementsByFlavor(flavor)) {
+            var filename = FilenameUtils.getName(element.getURI().getPath());
+            var uri = workspace.put(mpId, element.getIdentifier(), filename, workspace.read(element.getURI()));
+            element.setURI(uri);
+          }
+        }
+      }
+
       if (keepLastSnapshot) {
         logger.info("Deleting all but latest snapshot of episode {}", mpId);
+        deleted = q.delete(DEFAULT_OWNER, q.snapshot()).where(q.mediaPackageId(mpId).and(q.version().isLatest().not()))
+            .run();
+      } else if (rollbackVersion >= 0) {
+        logger.info("Rolling back to version {} in the asset manager", rollbackVersion);
         deleted = q.delete(DEFAULT_OWNER, q.snapshot())
-                .where(q.mediaPackageId(mpId).and(q.version().isLatest().not())).run();
+            .where(q.mediaPackageId(mpId)
+            .and(q.version().gt(new VersionImpl(rollbackVersion)))).run();
       } else {
         logger.info("Deleting all snapshots of episode {}", mpId);
         deleted = q.delete(DEFAULT_OWNER, q.snapshot())
