8c2dfba8c78fa44e4a1c971da0c0467036596e15
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2025-03-10 17:46:46 +0100

    Split DB migration script
    MariaDB and PostgresSQL use a different syntax for altering tables.

--- docs/upgrade/17_to_18/mariadb.sql
+++ docs/upgrade/17_to_18/mariadb.sql
@@ -1,5 +1,4 @@
--- Add creator_name and creator_username column to oc_series table
 ALTER TABLE oc_series ADD creator_username VARCHAR(128);
 ALTER TABLE oc_series ADD creator_name VARCHAR(255);
-UPDATE oc_series SET creator_username = '', creator_name = '';
+UPDATE oc_series SET creator_username = '', creator_name = '' WHERE creator_username IS NULL;
 ALTER TABLE oc_series MODIFY creator_username VARCHAR(128) NOT NULL;
---
+++ docs/upgrade/17_to_18/postgresql.sql
@@ -0,0 +1,4 @@
+ALTER TABLE oc_series ADD creator_username VARCHAR(128);
+ALTER TABLE oc_series ADD creator_name VARCHAR(255);
+UPDATE oc_series SET creator_username = '', creator_name = '' WHERE creator_username IS NULL;
+ALTER TABLE oc_series ALTER creator_username SET NOT NULL;
