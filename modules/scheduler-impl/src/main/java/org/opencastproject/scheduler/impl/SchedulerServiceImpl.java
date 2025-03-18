d67b99a3a78dc4d610e14152fe7a6bf9f6a9504e
Lars Kiesow <lkiesow@uos.de>
2025-03-18 23:45:40 +0100

    Log ID of event failing in scheduler index rebuild
    We had a few problems with rebuilding the scheduler index.
    Unfortunately, the logs just told us that the rebuild failed, not which
    event failed. That made debugging the error really hard.
    
    This patch adds the media package identifier to the error message, to
    avoid the same issue in the future.

--- modules/scheduler-impl/src/main/java/org/opencastproject/scheduler/impl/SchedulerServiceImpl.java
+++ modules/scheduler-impl/src/main/java/org/opencastproject/scheduler/impl/SchedulerServiceImpl.java
@@ -1752,6 +1752,8 @@ public class SchedulerServiceImpl extends AbstractIndexProducer implements Sched
                       }
                     } catch (SearchIndexException e) {
                       logger.error("Error while updating event '{}' from search index:", event.getMediaPackageId(), e);
+                    } catch (Exception e) {
+                      throw new RuntimeException("Fatal error while indexing event " + event.getMediaPackageId(), e);
                     }
                   }
                });
