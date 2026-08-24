97fc92b9e33d2ac90fd638c576bcf57eb0d344c9
Arnei <arnewilken@yahoo.de>
2026-08-24 12:30:25 +0200

    Add javadocs to lifecycle interfaces

--- modules/lifecycle-management-api/src/main/java/org/opencastproject/lifecyclemanagement/api/LifeCyclePolicy.java
+++ modules/lifecycle-management-api/src/main/java/org/opencastproject/lifecyclemanagement/api/LifeCyclePolicy.java
@@ -26,56 +26,190 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a life cycle policy in Opencast
+ */
 public interface LifeCyclePolicy {
+
+  /**
+   * Gets the policy's identifier
+   *
+   * @return the policy identifier
+   */
   String getId();
 
+  /**
+   * Sets the policy's identifier
+   *
+   * @param id the policy identifier
+   */
   void setId(String id);
 
+  /**
+   * Gets the policy's organization identifier
+   *
+   * @return the organization identifier
+   */
   String getOrganization();
 
+  /**
+   * Sets the policy's organization identifier
+   *
+   * @param organization the organization identifier
+   */
   void setOrganization(String organization);
 
+  /**
+   * Gets the policy's title
+   *
+   * @return the title
+   */
   String getTitle();
 
+  /**
+   * Sets the policy's title
+   *
+   * @param title the title
+   */
   void setTitle(String title);
 
+  /**
+   * Gets the type of entity this policy targets
+   *
+   * @return the {@link TargetType}
+   */
   TargetType getTargetType();
 
+  /**
+   * Sets the type of entity this policy targets
+   *
+   * @param targetType the {@link TargetType}
+   */
   void setTargetType(TargetType targetType);
 
+  /**
+   * Gets the action to be performed when the policy is applied
+   *
+   * @return the {@link Action}
+   */
   Action getAction();
 
+  /**
+   * Sets the action to be performed when the policy is applied
+   *
+   * @param action the {@link Action}
+   */
   void setAction(Action action);
 
+  /**
+   * Gets the parameters for the policy's {@link Action}
+   *
+   * @return the action parameters
+   */
   String getActionParameters();
 
+  /**
+   * Sets the parameters for the policy's {@link Action}
+   *
+   * @param actionParameters the action parameters
+   */
   void setActionParameters(String actionParameters);
 
+  /**
+   * Gets the date at which the policy's action should be performed
+   *
+   * @return the action date
+   */
   Date getActionDate();
 
+  /**
+   * Sets the date at which the policy's action should be performed
+   *
+   * @param actionDate the action date
+   */
   void setActionDate(Date actionDate);
 
+  /**
+   * Gets the cron expression that triggers the policy for a {@link Timing#REPEATING} policy
+   *
+   * @return the cron trigger expression
+   */
   String getCronTrigger();
 
+  /**
+   * Sets the cron expression that triggers the policy for a {@link Timing#REPEATING} policy
+   *
+   * @param cronTrigger the cron trigger expression
+   */
   void setCronTrigger(String cronTrigger);
 
+  /**
+   * Gets the policy's {@link Timing}
+   *
+   * @return the timing
+   */
   Timing getTiming();
 
+  /**
+   * Sets the policy's {@link Timing}
+   *
+   * @param timing the timing
+   */
   void setTiming(Timing timing);
 
-  boolean isActive() ;
+  /**
+   * Returns whether the policy is currently active
+   *
+   * @return true if the policy is active
+   */
+  boolean isActive();
 
+  /**
+   * Sets whether the policy is currently active
+   *
+   * @param active true if the policy is active
+   */
   void setActive(boolean active);
 
+  /**
+   * Returns whether the policy was created from a configuration file
+   *
+   * @return true if the policy was created from configuration
+   */
   boolean isCreatedFromConfig();
 
+  /**
+   * Sets whether the policy was created from a configuration file
+   *
+   * @param createdFromConfig true if the policy was created from configuration
+   */
   void setCreatedFromConfig(boolean createdFromConfig);
 
+  /**
+   * Gets the filters used to determine which entities this policy targets
+   *
+   * @return the target filters
+   */
   Map<String, Map<String, EventSearchQueryField<String>>> getTargetFilters();
 
+  /**
+   * Sets the filters used to determine which entities this policy targets
+   *
+   * @param targetFilters the target filters
+   */
   void setTargetFilters(Map<String, Map<String, EventSearchQueryField<String>>> targetFilters);
 
+  /**
+   * Gets the access control entries restricting who may manage this policy
+   *
+   * @return the list of {@link LifeCyclePolicyAccessControlEntry}s
+   */
   List<LifeCyclePolicyAccessControlEntry> getAccessControlEntries();
 
+  /**
+   * Sets the access control entries restricting who may manage this policy
+   *
+   * @param accessControlEntries the list of {@link LifeCyclePolicyAccessControlEntry}s
+   */
   void setAccessControlEntries(List<LifeCyclePolicyAccessControlEntry> accessControlEntries);
 }
--- modules/lifecycle-management-api/src/main/java/org/opencastproject/lifecyclemanagement/api/LifeCyclePolicyAccessControlEntry.java
+++ modules/lifecycle-management-api/src/main/java/org/opencastproject/lifecyclemanagement/api/LifeCyclePolicyAccessControlEntry.java
@@ -20,24 +20,78 @@
  */
 package org.opencastproject.lifecyclemanagement.api;
 
+/**
+ * Represents a single access control entry for a {@link LifeCyclePolicy}
+ */
 public interface LifeCyclePolicyAccessControlEntry {
+
+  /**
+   * Gets the entry's identifier
+   *
+   * @return the entry identifier
+   */
   long getId();
 
+  /**
+   * Sets the entry's identifier
+   *
+   * @param id the entry identifier
+   */
   void setId(long id);
 
+  /**
+   * Returns whether this entry allows or denies the action
+   *
+   * @return true if the action is allowed
+   */
   boolean isAllow();
 
+  /**
+   * Sets whether this entry allows or denies the action
+   *
+   * @param allow true if the action is allowed
+   */
   void setAllow(boolean allow);
 
+  /**
+   * Gets the role this entry applies to
+   *
+   * @return the role
+   */
   String getRole();
 
+  /**
+   * Sets the role this entry applies to
+   *
+   * @param role the role
+   */
   void setRole(String role);
 
+  /**
+   * Gets the action this entry allows or denies
+   *
+   * @return the action
+   */
   String getAction();
 
+  /**
+   * Sets the action this entry allows or denies
+   *
+   * @param action the action
+   */
   void setAction(String action);
 
+  /**
+   * Converts this entry to an {@link org.opencastproject.security.api.AccessControlEntry}
+   *
+   * @return the converted access control entry
+   */
   org.opencastproject.security.api.AccessControlEntry toAccessControlEntry();
 
+  /**
+   * Populates this entry from an {@link org.opencastproject.security.api.AccessControlEntry}
+   *
+   * @param accessControlEntry the access control entry to populate from
+   */
   void fromAccessControlEntry(org.opencastproject.security.api.AccessControlEntry accessControlEntry);
 }
